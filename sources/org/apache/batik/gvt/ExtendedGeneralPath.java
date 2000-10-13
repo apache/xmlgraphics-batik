01841e72c5e47b02475fdc8f443760b0e8e01638
Thierry Kormann <tkormann@apache.org>
2000-10-13 13:27:37 +0000

    bug fix with arcTo PR: Obtained from: Submitted by: Reviewed by: PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@198797 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/gvt/ExtendedGeneralPath.java
+++ sources/org/apache/batik/gvt/ExtendedGeneralPath.java
@@ -33,33 +33,33 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
      * Constructs a new <code>ExtendedGeneralPath</code>.
      */
     public ExtendedGeneralPath() {
-	path = new GeneralPath();
+        path = new GeneralPath();
     }
 
     /**
      * Constructs a new <code>ExtendedGeneralPath</code> with the
      * specified winding rule to control operations that require the
-     * interior of the path to be defined.  
+     * interior of the path to be defined.
      */
     public ExtendedGeneralPath(int rule) {
-	path = new GeneralPath(rule);
+        path = new GeneralPath(rule);
     }
 
     /**
      * Constructs a new <code>ExtendedGeneralPath</code> object with
      * the specified winding rule and the specified initial capacity
-     * to store path coordinates. 
+     * to store path coordinates.
      */
     public ExtendedGeneralPath(int rule, int initialCapacity) {
-	path = new GeneralPath(rule, initialCapacity);
+        path = new GeneralPath(rule, initialCapacity);
     }
 
     /**
      * Constructs a new <code>ExtendedGeneralPath</code> object from
-     * an arbitrary <code>Shape</code> object.  
+     * an arbitrary <code>Shape</code> object.
      */
     public ExtendedGeneralPath(Shape s) {
-	path = new GeneralPath(s);
+        path = new GeneralPath(s);
     }
 
     /**
@@ -68,7 +68,7 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
      * indicate if we increase or decrease the angles and the final
      * point of the arc.
      *
-     * @param rx,&nbsp;ry the radii of the ellipse 
+     * @param rx,&nbsp;ry the radii of the ellipse
      *
      * @param theta the angle from the x-axis of the current
      * coordinate system to the x-axis of the ellipse in degrees.
@@ -84,129 +84,127 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
      * @param x,&nbsp;y the absolute coordinates of the final point of
      * the arc.
      */
-    public synchronized void arcTo(float rx, float ry, 
-				   float theta, 
-				   boolean largeArcFlag, 
-				   boolean sweepFlag,
-				   float x, float y) {
-	//
-	// Elliptical arc implementation based on the SVG specification notes 
-	//
+    public synchronized void arcTo(double rx, double ry,
+                                   double theta,
+                                   boolean largeArcFlag,
+                                   boolean sweepFlag,
+                                   double x, double y) {
+        //
+        // Elliptical arc implementation based on the SVG specification notes
+        //
 
-	// Ensure radii are valid
-	if (rx == 0 || ry == 0) {
-	    lineTo(x ,y);
-	    return;
-	}
-	// Get the current (x, y) coordinates of the path
-	Point2D p2d = path.getCurrentPoint();
-	float x0 = (float) p2d.getX();
-	float y0 = (float) p2d.getY();
-	// Compute the half distance between the current and the final point
-	float dx2 = (x0 - x) / 2.0f;
-	float dy2 = (y0 - y) / 2.0f;
-	// Convert theta from degrees to radians
-	theta = (float) Math.toRadians(theta % 360f);
+        // Ensure radii are valid
+        if (rx == 0 || ry == 0) {
+            lineTo((float) x, (float) y);
+            return;
+        }
+        // Get the current (x, y) coordinates of the path
+        Point2D p2d = path.getCurrentPoint();
+        double x0 = p2d.getX();
+        double y0 = p2d.getY();
+        // Compute the half distance between the current and the final point
+        double dx2 = (x0 - x) / 2.0;
+        double dy2 = (y0 - y) / 2.0;
+        // Convert theta from degrees to radians
+        theta = Math.toRadians(theta % 360.0);
 
-	//
-	// Step 1 : Compute (x1, y1)
-	//
-	float x1 = (float) (Math.cos(theta) * (double) dx2 + 
-			    Math.sin(theta) * (double) dy2);
-	float y1 = (float) (-Math.sin(theta) * (double) dx2 + 
-			    Math.cos(theta) * (double) dy2);
-	// Ensure radii are large enough
-	rx = Math.abs(rx);
-	ry = Math.abs(ry);
-	float Prx = rx * rx;
-	float Pry = ry * ry;
-	float Px1 = x1 * x1;
-	float Py1 = y1 * y1;
-	double d = Px1/Prx + Py1/Pry;
-	if (d > 1) {
-	    rx = Math.abs((float) (Math.sqrt(d) * (double) rx));
-	    ry = Math.abs((float) (Math.sqrt(d) * (double) ry));
-	    Prx = rx * rx;
-	    Pry = ry * ry;
-	}
+        //
+        // Step 1 : Compute (x1, y1)
+        //
+        double x1 = (Math.cos(theta) * dx2 + Math.sin(theta) * dy2);
+        double y1 = (-Math.sin(theta) * dx2 + Math.cos(theta) * dy2);
+        // Ensure radii are large enough
+        rx = Math.abs(rx);
+        ry = Math.abs(ry);
+        double Prx = rx * rx;
+        double Pry = ry * ry;
+        double Px1 = x1 * x1;
+        double Py1 = y1 * y1;
+        double d = Px1/Prx + Py1/Pry;
+        if (d > 1) {
+            rx = Math.abs((Math.sqrt(d) * rx));
+            ry = Math.abs((Math.sqrt(d) * ry));
+            Prx = rx * rx;
+            Pry = ry * ry;
+        }
 
-	//
-	// Step 2 : Compute (cx1, cy1)
-	//
-	double sign = (largeArcFlag == sweepFlag)?-1d:1d;
-	float coef = (float) (sign * Math.sqrt(((Prx*Pry)-(Prx*Py1)-(Pry*Px1)) /
-					       ((Prx*Py1)+(Pry*Px1))));
-	float cx1 = coef * ((rx * y1) / ry);
-	float cy1 = coef * -((ry * x1) / rx);
+        //
+        // Step 2 : Compute (cx1, cy1)
+        //
+        double sign = (largeArcFlag == sweepFlag) ? -1d : 1d;
+        double coef = (sign * Math.sqrt(((Prx*Pry)-(Prx*Py1)-(Pry*Px1)) /
+                                        ((Prx*Py1)+(Pry*Px1))));
+        double cx1 = coef * ((rx * y1) / ry);
+        double cy1 = coef * -((ry * x1) / rx);
 
-	//
-	// Step 3 : Compute (cx, cy) from (cx1, cy1)
-	//
-	float sx2 = (x0 + x) / 2.0f;
-	float sy2 = (y0 + y) / 2.0f;
-	float cx = sx2 + (float) (Math.cos(theta) * (double) cx1 - 
-				  Math.sin(theta) * (double) cy1);
-	float cy = sy2 + (float) (Math.sin(theta) * (double) cx1 + 
-				  Math.cos(theta) * (double) cy1);
+        //
+        // Step 3 : Compute (cx, cy) from (cx1, cy1)
+        //
+        double sx2 = (x0 + x) / 2.0;
+        double sy2 = (y0 + y) / 2.0;
+        double cx = sx2 + (Math.cos(theta) * cx1 - Math.sin(theta) * cy1);
+        double cy = sy2 + (Math.sin(theta) * cx1 + Math.cos(theta) * cy1);
 
-	//
-	// Step 4 : Compute the angleStart (theta1) and the angleExtent (dtheta)
-	//
-	float ux = (x1 - cx1) / rx;
-	float uy = (y1 - cy1) / ry;
-	float vx = (-x1 - cx1) / rx;
-	float vy = (-y1 - cy1) / ry;
-	float p, n;
-	// Compute the angle start
-	n = (float) Math.sqrt((ux * ux) + (uy * uy));
-	p = ux; // (1 * ux) + (0 * uy)
-	sign = (uy < 0)?-1d:1d;
-	float angleStart = (float) Math.toDegrees(sign * Math.acos(p / n));
-	// Compute the angle extent
-	n = (float) Math.sqrt((ux * ux + uy * uy) * (vx * vx + vy * vy));
-	p = ux * vx + uy * vy;
-	sign = (ux * vy - uy * vx < 0)?-1d:1d;
-	float angleExtent = (float) Math.toDegrees(sign * Math.acos(p / n));
-	if(!sweepFlag && angleExtent > 0) {
-	    angleExtent -= 360f;
-	} else if (sweepFlag && angleExtent < 0) {
-	    angleExtent += 360f;
-	}
-	angleExtent %= 360f;
-	angleStart %= 360f;
+        //
+        // Step 4 : Compute the angleStart (theta1) and the angleExtent (dtheta)
+        //
+        double ux = (x1 - cx1) / rx;
+        double uy = (y1 - cy1) / ry;
+        double vx = (-x1 - cx1) / rx;
+        double vy = (-y1 - cy1) / ry;
+        double p, n;
+        // Compute the angle start
+        n = Math.sqrt((ux * ux) + (uy * uy));
+        p = ux; // (1 * ux) + (0 * uy)
+        sign = (uy < 0) ? -1d : 1d;
+        double angleStart = Math.toDegrees(sign * Math.acos(p / n));
+        // Compute the angle extent
+        n = Math.sqrt((ux * ux + uy * uy) * (vx * vx + vy * vy));
+        p = ux * vx + uy * vy;
+        sign = (ux * vy - uy * vx < 0) ? -1d : 1d;
+        double angleExtent = Math.toDegrees(sign * Math.acos(p / n));
+        if(!sweepFlag && angleExtent > 0) {
+            angleExtent -= 360f;
+        } else if (sweepFlag && angleExtent < 0) {
+            angleExtent += 360f;
+        }
+        angleExtent %= 360f;
+        angleStart %= 360f;
 
-	//
-	// We can now build the resulting Arc2D in float precision
-	//
-	Arc2D.Float arc = new Arc2D.Float();
-	arc.x = cx - rx;
-	arc.y = cy - ry;
-	arc.width = rx * 2.0f;
-	arc.height = ry * 2.0f;
-	arc.start = -angleStart;
-	arc.extent = -angleExtent;
-	append(arc, true);
+        //
+        // We can now build the resulting Arc2D in double precision
+        //
+        Arc2D.Double arc = new Arc2D.Double();
+        arc.x = cx - rx;
+        arc.y = cy - ry;
+        arc.width = rx * 2.0;
+        arc.height = ry * 2.0;
+        arc.start = -angleStart;
+        arc.extent = -angleExtent;
+        AffineTransform t = AffineTransform.getRotateInstance(theta, cx, cy);
+        Shape s = t.createTransformedShape(arc);
+        append(s, true);
     }
 
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public synchronized void moveTo(float x, float y) {
-	path.moveTo(x, y);
+        path.moveTo(x, y);
     }
 
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public synchronized void lineTo(float x, float y) {
-	path.lineTo(x, y);
+        path.lineTo(x, y);
     }
 
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public synchronized void quadTo(float x1, float y1, float x2, float y2) {
-	path.quadTo(x1, y1, x2, y2);
+        path.quadTo(x1, y1, x2, y2);
     }
 
     /**
@@ -215,28 +213,28 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
     public synchronized void curveTo(float x1, float y1,
                                      float x2, float y2,
                                      float x3, float y3) {
-	path.curveTo(x1, y1, x2, y2, x3, y3);
+        path.curveTo(x1, y1, x2, y2, x3, y3);
     }
 
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public synchronized void closePath() {
-	path.closePath();
+        path.closePath();
     }
 
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public void append(Shape s, boolean connect) {
-	path.append(s, connect);
+        path.append(s, connect);
     }
 
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public void append(PathIterator pi, boolean connect) {
-	path.append(pi, connect);
+        path.append(pi, connect);
     }
 
     /**
@@ -250,14 +248,14 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public void setWindingRule(int rule) {
-	path.setWindingRule(rule);
+        path.setWindingRule(rule);
     }
 
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public synchronized Point2D getCurrentPoint() {
-	return path.getCurrentPoint();
+        return path.getCurrentPoint();
     }
 
     /**
@@ -278,7 +276,7 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public synchronized Shape createTransformedShape(AffineTransform at) {
-	return path.createTransformedShape(at);
+        return path.createTransformedShape(at);
     }
 
     /**
@@ -292,14 +290,14 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public synchronized Rectangle2D getBounds2D() {
-	return path.getBounds2D();
+        return path.getBounds2D();
     }
 
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public boolean contains(double x, double y) {
-	return path.contains(x, y);
+        return path.contains(x, y);
     }
 
     /**
@@ -313,7 +311,7 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public boolean contains(double x, double y, double w, double h) {
-	return path.contains(x, y, w, h);
+        return path.contains(x, y, w, h);
     }
 
     /**
@@ -327,7 +325,7 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public boolean intersects(double x, double y, double w, double h) {
-	return path.intersects(x, y, w, h);
+        return path.intersects(x, y, w, h);
     }
 
     /**
@@ -350,16 +348,16 @@ public class ExtendedGeneralPath implements Shape, Cloneable {
     public PathIterator getPathIterator(AffineTransform at, double flatness) {
         return path.getPathIterator(at, flatness);
     }
- 
+
     /**
      * Delegates to the enclosed <code>GeneralPath</code>.
      */
     public Object clone() {
-	try {
-	    ExtendedGeneralPath result = (ExtendedGeneralPath) super.clone();
-	    result.path = (GeneralPath) path.clone();
-	    return result;
-	} catch (CloneNotSupportedException ex) {}
-	return null;
+        try {
+            ExtendedGeneralPath result = (ExtendedGeneralPath) super.clone();
+            result.path = (GeneralPath) path.clone();
+            return result;
+        } catch (CloneNotSupportedException ex) {}
+        return null;
     }
 }
