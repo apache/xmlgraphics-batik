56861a10ae4a9a7a7638727b834aa106a54c3e63
Stefeno Mazzocchi <stefano@apache.org>
2000-10-28 23:02:31 +0000

    added maintain doc and build scripts (still working on the build file)
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@198885 13f79535-47bb-0310-9956-ffa450edef68

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# -----------------------------------------------------------------------------
+# build.sh - Unix Build Script for Apache Batik
+#
+# $Id: build.sh,v 1.10.2.13 2000/09/28 19:53:04 rossb Exp $
+# -----------------------------------------------------------------------------
+
+# ----- Verify and Set Required Environment Variables -------------------------
+   
+if [ "$ANT_HOME" = "" ] ; then
+  ANT_HOME=./lib
+fi
+
+if [ "$JAVA_HOME" = "" ] ; then
+  echo You must set JAVA_HOME to point at your Java Development Kit installation
+  exit 1
+fi
+
+# ----- Set Up The Runtime Classpath ------------------------------------------
+
+CP=$JAVA_HOME/lib/tools.jar:$ANT_HOME/lib/ant_1_2.jar:./lib/parser.jar:./lib/jaxp.jar
+ 
+# ----- Make sure Ant script is executable ------------------------------------
+
+chmod 0755 $ANT_HOME/bin/antRun
+
+# ----- Execute The Requested Build -------------------------------------------
+
+$JAVA_HOME/bin/java $ANT_OPTS -classpath $CP org.apache.tools.ant.Main -Dant.home=$ANT_HOME $*
+
+
+
