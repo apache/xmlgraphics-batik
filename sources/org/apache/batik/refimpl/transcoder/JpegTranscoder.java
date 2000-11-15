3a764b201208bf122222cf94fd158a623414b8e8
Thierry Kormann <tkormann@apache.org>
2000-11-15 12:28:18 +0000

    add some javadoc to transcoders PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199072 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/refimpl/transcoder/JpegTranscoder.java
+++ sources/org/apache/batik/refimpl/transcoder/JpegTranscoder.java
@@ -18,22 +18,35 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * An <tt>ImageTranscoder</tt> that produces a jpeg image.
+ * An <tt>ImageTranscoder</tt> that produces a jpeg image. The default
+ * background color is white.
  *
  * @author <a href="mailto:Thierry.Kormann@sophia.inria.fr">Thierry Kormann</a>
  * @version $Id$
  */
 public class JpegTranscoder extends ImageTranscoder {
 
+    /**
+     * Constructs a new jpeg transcoder.
+     */
     public JpegTranscoder(){
-        hints.put(TranscodingHints.KEY_BACKGROUND,
-                  Color.white);
+        hints.put(TranscodingHints.KEY_BACKGROUND, Color.white);
     }
 
+    /**
+     * Creates a new image of type RGB.
+     * @param w the width of the image
+     * @param h the height of the image
+     */
     public BufferedImage createImage(int w, int h){
         return new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
     }
 
+    /**
+     * Writes the specified image as a jpeg to the specified ouput stream.
+     * @param img the image to write
+     * @param ostream the output stream where to write the image
+     */
     public void writeImage(BufferedImage img, OutputStream ostream)
             throws IOException {
         JPEGImageEncoder jpegEncoder = JPEGCodec.createJPEGEncoder(ostream);
@@ -42,6 +55,9 @@ public class JpegTranscoder extends ImageTranscoder {
         jpegEncoder.encode(img, params);
     }
 
+    /**
+     * Returns the jpeg mime type <tt>image/jpeg</tt>.
+     */
     public String getMimeType() {
         return "image/jpeg";
     }
