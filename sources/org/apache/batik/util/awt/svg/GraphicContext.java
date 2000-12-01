0ce38a8c1ee50c5fa91c112d9a195c0d4773b07f
Vincent Hardy <vhardy@apache.org>
2000-12-01 17:35:09 +0000

    Removed remaining javadoc comments.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199234 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/util/awt/svg/GraphicContext.java
+++ sources/org/apache/batik/util/awt/svg/GraphicContext.java
@@ -361,7 +361,6 @@ public class GraphicContext implements Cloneable{
      * @see java.awt.Graphics#setPaintMode
      * @see java.awt.AlphaComposite
      * @see java.lang.SecurityManager#checkPermission(java.awt.Permission)
-     * @see java.awt.AWTPermission
      */
     public void setComposite(Composite comp){
         this.composite = comp;
