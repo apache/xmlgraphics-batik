bc8b6e41e450c5931102ab8d81d9e343e6ec3767
Christophe Jolif <cjolif@apache.org>
2001-01-19 17:37:05 +0000

    minor cleanups
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199354 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/refimpl/script/jacl/JaclInterpreter.java
+++ sources/org/apache/batik/refimpl/script/jacl/JaclInterpreter.java
@@ -1,6 +1,6 @@
 /*****************************************************************************
  * Copyright (C) The Apache Software Foundation. All rights reserved.        *
- *---------------------------------------------------------------------------*  
+ *---------------------------------------------------------------------------*
  * This software is published under the terms of the Apache Software License *
  * version 1.1, a copy of which has been included with this distribution in  *
  * the LICENSE file.                                                         *
@@ -13,7 +13,6 @@ import java.util.Locale;
 import org.apache.batik.script.*;
 import tcl.lang.*;
 
-
 /**
  * A simple implementation of <code>Interpreter</code> interface to use
  * JACL Tcl parser.
@@ -22,28 +21,18 @@ import tcl.lang.*;
  */
 public class JaclInterpreter implements org.apache.batik.script.Interpreter {
     private Interp interpreter = null;
-    
+
     public JaclInterpreter() {
         interpreter = new Interp();
         try {
             interpreter.eval("package require java", 0);
         } catch (TclException e) {
         }
-        // import Java lang package & DOM Level 2 & SVG DOM packages
-        //        JavaImportCmd import = new JavaImportCmd();
-        //        import.cmdProc();
-        //        pkg = new NativeJavaPackage("org.w3c.dom");
-        //        pkg = new NativeJavaPackage("org.w3c.dom.css");
-        //        pkg = new NativeJavaPackage("org.w3c.dom.events");
-        //        pkg = new NativeJavaPackage("org.w3c.dom.smil");
-        //        pkg = new NativeJavaPackage("org.w3c.dom.stylesheets");
-        //        pkg = new NativeJavaPackage("org.w3c.dom.svg");
-        //        pkg = new NativeJavaPackage("org.w3c.dom.views");
     }
 
     // org.apache.batik.script.Intepreter implementation
-    
-    public Object evaluate(Reader scriptreader) 
+
+    public Object evaluate(Reader scriptreader)
         throws InterpreterException, IOException {
         try {
             // oups jacl doesn't accept reader in its eval method :-(
@@ -56,33 +45,33 @@ public class JaclInterpreter implements org.apache.batik.script.Interpreter {
             String str = sbuffer.toString();
             interpreter.eval(str, 0);
         } catch (TclException e) {
-            e.printStackTrace();
             throw new InterpreterException(e, e.getMessage(), -1, -1);
         } catch (RuntimeException re) {
             throw new InterpreterException(re, re.getMessage(), -1, -1);
         }
         return interpreter.getResult();
     }
-    
+
     public void dispose() {
         interpreter.dispose();
     }
-    
+
     public void bindObject(String name, Object object) {
         try {
             interpreter.
                 setVar(name,
-                       ReflectObject.newInstance(interpreter, object.getClass(), object),
+                       ReflectObject.
+                       newInstance(interpreter, object.getClass(), object),
                        0);
         } catch (TclException e) {
-            // 
+            // should not happened we just register an object
         }
-    } 
-    
+    }
+
     public void setOut(Writer out) {
         // no implementation of a default output function in Jacl
     }
-    
+
     // org.apache.batik.i18n.Localizable implementation
 
     public Locale getLocale() {
@@ -91,7 +80,7 @@ public class JaclInterpreter implements org.apache.batik.script.Interpreter {
 
     public void setLocale(Locale locale) {
     }
-    
+
     public String formatMessage(String key, Object[] args) {
         return null;
     }
