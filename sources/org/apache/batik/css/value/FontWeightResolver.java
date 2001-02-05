cef958136a6dc87aa5c17f5a9b9ba91b7f902616
billh <billh@apache.org>
2001-02-05 15:20:46 +0000

    Patch to resolve relative font weights that gives "intended" resutls for "bolder" and "lighter"...always resolves "lighter" to "400" for bold fonts (>500) and resolves "bolder" to 600 for non-bold fonts (<=500). This matches our weight tagging in the SVGTextElementBridge, since we don't now use intermediate-boldness tags like ULTRABOLD, DEMIBOLD. (Though Java defines these weights, they don't work with standard fonts). NOTE: Relative font weight resolution should eventually move to the renderer.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199450 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/css/value/FontWeightResolver.java
+++ sources/org/apache/batik/css/value/FontWeightResolver.java
@@ -28,23 +28,23 @@ public class FontWeightResolver implements RelativeValueResolver {
      * Whether the handled property is inherited or not.
      */
     public boolean isInheritedProperty() {
-	return true;
+        return true;
     }
 
     /**
      * Returns the name of the handled property.
      */
     public String getPropertyName() {
-	return ValueConstants.CSS_FONT_WEIGHT_PROPERTY;
+        return ValueConstants.CSS_FONT_WEIGHT_PROPERTY;
     }
 
     /**
      * Returns the default value for the handled property.
      */
     public CSSOMReadOnlyValue getDefaultValue() {
-	return new CSSOMReadOnlyValue(ValueConstants.NORMAL_VALUE);
+        return new CSSOMReadOnlyValue(ValueConstants.NORMAL_VALUE);
     }
-    
+
     /**
      * Resolves the given value if relative, and puts it in the given table.
      * @param element The element to which this value applies.
@@ -56,82 +56,90 @@ public class FontWeightResolver implements RelativeValueResolver {
      * @param origin The origin of the cascaded value.
      */
     public void resolveValue(Element element,
-			     String pseudoElement,
-			     ViewCSS view,
-			     CSSOMReadOnlyStyleDeclaration styleDeclaration,
-			     CSSOMReadOnlyValue value,
-			     String priority,
-			     int origin) {
-	ImmutableValue im = value.getImmutableValue();
-	boolean b = im == ValueConstants.BOLDER_VALUE;
-	if (b || im == ValueConstants.LIGHTER_VALUE) {
-	    Element p = HiddenChildElementSupport.getParentElement(element);
-	    CSSOMReadOnlyValue val;
-	    if (p == null) {
-		val = new CSSOMReadOnlyValue((b)
-					     ? ValueConstants.NUMBER_500
-					     : ValueConstants.NUMBER_300);
-	    } else {
-		CSSOMReadOnlyStyleDeclaration sd;
-		sd = (CSSOMReadOnlyStyleDeclaration)view.getComputedStyle
+                             String pseudoElement,
+                             ViewCSS view,
+                             CSSOMReadOnlyStyleDeclaration styleDeclaration,
+                             CSSOMReadOnlyValue value,
+                             String priority,
+                             int origin) {
+        ImmutableValue im = value.getImmutableValue();
+        /**
+         * Note:  this implementation should change, since the spec says
+         * the next lighter/bolder "available" font should be chosen, and
+         * that if none available then increment/decrement by 100.
+         * Since at the moment we only have normal and bold fonts,
+         * the interim solution is a reasonable "fit".  Eventually this
+         * resolution must be done in the renderer instead of here.
+         */
+        boolean b = im == ValueConstants.BOLDER_VALUE;
+        if (b || im == ValueConstants.LIGHTER_VALUE) {
+            Element p = HiddenChildElementSupport.getParentElement(element);
+            CSSOMReadOnlyValue val;
+            if (p == null) {
+                val = new CSSOMReadOnlyValue((b)
+                                             ? ValueConstants.NUMBER_600
+                                             : ValueConstants.NUMBER_300);
+            } else {
+                CSSOMReadOnlyStyleDeclaration sd;
+                sd = (CSSOMReadOnlyStyleDeclaration)view.getComputedStyle
                     (p, null);
-		CSSOMReadOnlyValue prop;
-		prop = (CSSOMReadOnlyValue)sd.getPropertyCSSValue
+                CSSOMReadOnlyValue prop;
+                prop = (CSSOMReadOnlyValue)sd.getPropertyCSSValue
                     (getPropertyName());
-		im = prop.getImmutableValue();
-		if (im == ValueConstants.NUMBER_100) {
-		    val = new CSSOMReadOnlyValue
+                im = prop.getImmutableValue();
+                if (im == ValueConstants.NUMBER_100) {
+                    val = new CSSOMReadOnlyValue
                         ((b)
-                         ? ValueConstants.NUMBER_200
+                         ? ValueConstants.NUMBER_600
                          : ValueConstants.NUMBER_100);
-		} else if (im == ValueConstants.NUMBER_200) {
-		    val = new CSSOMReadOnlyValue
+                } else if (im == ValueConstants.NUMBER_200) {
+                    val = new CSSOMReadOnlyValue
                         ((b)
-                         ? ValueConstants.NUMBER_300
+                         ? ValueConstants.NUMBER_600
                          : ValueConstants.NUMBER_100);
-		} else if (im == ValueConstants.NUMBER_300) {
-		    val = new CSSOMReadOnlyValue
+                } else if (im == ValueConstants.NUMBER_300) {
+                    val = new CSSOMReadOnlyValue
                         ((b)
-                         ? ValueConstants.NUMBER_400
+                         ? ValueConstants.NUMBER_600
                          : ValueConstants.NUMBER_200);
-		} else if (im == ValueConstants.NUMBER_400 ||
-			   im == ValueConstants.NORMAL_VALUE) {
-		    val = new CSSOMReadOnlyValue
+                } else if (im == ValueConstants.NUMBER_400 ||
+                           im == ValueConstants.NORMAL_VALUE) {
+                    val = new CSSOMReadOnlyValue
                         ((b)
-                         ? ValueConstants.NUMBER_500
+                         ? ValueConstants.NUMBER_600
                          : ValueConstants.NUMBER_300);
-		} else if (im == ValueConstants.NUMBER_500) {
-		    val = new CSSOMReadOnlyValue
+                } else if (im == ValueConstants.NUMBER_500) {
+                    val = new CSSOMReadOnlyValue
                         ((b)
                          ? ValueConstants.NUMBER_600
                          : ValueConstants.NUMBER_400);
-		} else if (im == ValueConstants.NUMBER_600) {
-		    val = new CSSOMReadOnlyValue
+                } else if (im == ValueConstants.NUMBER_600) {
+                    val = new CSSOMReadOnlyValue
                         ((b)
                          ? ValueConstants.NUMBER_700
-                         : ValueConstants.NUMBER_500);
-		} else if (im == ValueConstants.NUMBER_700 ||
-			   im == ValueConstants.BOLD_VALUE) {
-		    val = new CSSOMReadOnlyValue
+                         : ValueConstants.NUMBER_400);
+                } else if (im == ValueConstants.NUMBER_700 ||
+                           im == ValueConstants.BOLD_VALUE) {
+                    val = new CSSOMReadOnlyValue
                         ((b)
                          ? ValueConstants.NUMBER_800
-                         : ValueConstants.NUMBER_600);
-		} else if (im == ValueConstants.NUMBER_800) {
-		    val = new CSSOMReadOnlyValue
+                         : ValueConstants.NUMBER_400);
+                } else if (im == ValueConstants.NUMBER_800) {
+                    val = new CSSOMReadOnlyValue
                         ((b)
                          ? ValueConstants.NUMBER_900
-                         : ValueConstants.NUMBER_700);
-		} else {
-		    val = new CSSOMReadOnlyValue
+                         : ValueConstants.NUMBER_400);
+                } else {
+                    val = new CSSOMReadOnlyValue
                         ((b)
                          ? ValueConstants.NUMBER_900
-                         : ValueConstants.NUMBER_800);
-		}
-	    }
-	    styleDeclaration.setPropertyCSSValue(getPropertyName(),
-						 val,
-						 priority,
-						 origin);
-	}
+                         : ValueConstants.NUMBER_400);
+                }
+            }
+            styleDeclaration.setPropertyCSSValue(getPropertyName(),
+                                                 val,
+                                                 priority,
+                                                 origin);
+        }
     }
 }
