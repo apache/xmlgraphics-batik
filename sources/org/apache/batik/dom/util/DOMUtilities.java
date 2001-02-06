75e8a3c238ffac7bb71ef8dad8638fc0dd406301
Stephane Hillion <hillion@apache.org>
2001-02-06 14:17:04 +0000

    The SVG transcoder can now be used for DOM serialization. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199462 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/dom/util/DOMUtilities.java
+++ sources/org/apache/batik/dom/util/DOMUtilities.java
@@ -8,7 +8,11 @@
 
 package org.apache.batik.dom.util;
 
+import java.io.IOException;
+import java.io.Writer;
+
 import org.apache.batik.util.XMLUtilities;
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.DOMException;
@@ -28,6 +32,118 @@ public class DOMUtilities extends XMLUtilities {
      * Do not need to be instantiated.
      */
     protected DOMUtilities() {
+    }
+
+    /**
+     * Writes the given document using the given writer.
+     */
+    public static void writeDocument(Document doc, Writer w) throws IOException {
+        for (Node n = doc.getFirstChild();
+             n != null;
+             n = n.getNextSibling()) {
+            writeNode(n, w);
+        }
+    }
+
+    /**
+     * Writes a node using the given writer.
+     */
+    public static void writeNode(Node n, Writer w) throws IOException {
+        switch (n.getNodeType()) {
+        case Node.ELEMENT_NODE:
+            w.write("<");
+            w.write(n.getNodeName());
+
+            if (n.hasAttributes()) {
+                NamedNodeMap attr = n.getAttributes();
+                int len = attr.getLength();
+                for (int i = 0; i < len; i++) {
+                    Attr a = (Attr)attr.item(i);
+                    w.write(" ");
+                    w.write(a.getNodeName());
+                    w.write("=\"");
+                    w.write(contentToString(a.getNodeValue()));
+                    w.write("\"");
+                }
+            }
+
+            Node c = n.getFirstChild();
+            if (c != null) {
+                w.write(">");
+                for (; c != null;
+                     c = c.getNextSibling()) {
+                    writeNode(c, w);
+                }
+                w.write("</");
+                w.write(n.getNodeName());
+                w.write(">");
+            } else {
+                w.write("/>");
+            }
+            break;
+        case Node.TEXT_NODE:
+            w.write(contentToString(n.getNodeValue()));
+            break;
+        case Node.CDATA_SECTION_NODE:
+            w.write("<![CDATA[");
+            w.write(n.getNodeValue());
+            w.write("]]>");
+            break;
+        case Node.ENTITY_REFERENCE_NODE:
+            w.write("&");
+            w.write(n.getNodeName());
+            w.write(";");
+            break;
+        case Node.PROCESSING_INSTRUCTION_NODE:
+            w.write("<?");
+            w.write(n.getNodeName());
+            w.write(n.getNodeValue());
+            w.write("?>");
+            break;
+        case Node.COMMENT_NODE:
+            w.write("<!--");
+            w.write(n.getNodeValue());
+            w.write("-->");
+            break;
+        case Node.DOCUMENT_TYPE_NODE:
+            break;
+        default:
+            throw new Error("Internal error (" + n.getNodeType() + ")");
+        }
+    }
+
+    /**
+     * Returns the given content value transformed to replace invalid
+     * characters with entities.
+     */
+    public static String contentToString(String s) {
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            switch (c) {
+            case '<':
+                result.append("&lt;");
+                break;
+            case '>':
+                result.append("&gt;");
+                break;
+            case '&':
+                result.append("&amp;");
+                break;
+            case '"':
+                result.append("&quot;");
+                break;
+            case '\'':
+                result.append("&apos;");
+                break;
+            default:
+                result.append(c);
+            }
+        }
+        
+        return result.toString();
     }
 
     /**
@@ -99,7 +215,7 @@ public class DOMUtilities extends XMLUtilities {
         case Node.COMMENT_NODE:
             return doc.createComment(n.getNodeValue());
         default:
-            throw  new Error("Internal error (" + n.getNodeType() + ")");
+            throw new Error("Internal error (" + n.getNodeType() + ")");
         }
     }
 
