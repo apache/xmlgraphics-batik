66876420ec340d0c511b0e05f819118b0d089068
Thierry Kormann <tkormann@apache.org>
2001-03-15 18:49:29 +0000

    add bug :) PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199581 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/gvt/AbstractGraphicsNode.java
+++ sources/org/apache/batik/gvt/AbstractGraphicsNode.java
@@ -851,6 +851,10 @@ public abstract class AbstractGraphicsNode implements GraphicsNode, Cloneable {
         // Get the primitive bounds
         // Rectangle2D bounds = null;
         if(bounds == null){
+            // first, make sure we haven't been interrupted
+            if (Thread.currentThread().isInterrupted()) {
+                return null;
+            }
             // The painted region, before cliping, masking and
             // compositing is either the area painted by the primitive
             // paint or the area painted by the filter.
