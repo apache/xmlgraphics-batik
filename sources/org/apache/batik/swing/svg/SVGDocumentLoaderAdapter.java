e503cf34022865cb667da7344f17e284be91674b
Thierry Kormann <tkormann@apache.org>
2001-04-13 17:09:09 +0000

    bug fix when trying to see a DOM tree (without starting from a URI) PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199692 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sources/org/apache/batik/swing/svg/SVGDocumentLoaderAdapter.java
@@ -0,0 +1,40 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.swing.svg;
+
+/**
+ * An adapter class that represents a listener to the SVGDocumentLoaderEvent
+ * events.
+ *
+ * @author <a href="mailto:tkormann@apache.org">Thierry Kormann</a>
+ * @version $Id$
+ */
+public abstract class SVGDocumentLoaderAdapter
+    implements SVGDocumentLoaderListener {
+
+    /**
+     * Called when the loading of a document was started.
+     */
+    public void documentLoadingStarted(SVGDocumentLoaderEvent e) {}
+
+    /**
+     * Called when the loading of a document was completed.
+     */
+    public void documentLoadingCompleted(SVGDocumentLoaderEvent e) {}
+
+    /**
+     * Called when the loading of a document was cancelled.
+     */
+    public void documentLoadingCancelled(SVGDocumentLoaderEvent e) {}
+
+    /**
+     * Called when the loading of a document has failed.
+     */
+    public void documentLoadingFailed(SVGDocumentLoaderEvent e) {}
+}
