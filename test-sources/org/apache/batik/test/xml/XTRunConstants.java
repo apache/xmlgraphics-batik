ad55eed213c979a61a6454a65948cffbbc7d3269
Vincent Hardy <vhardy@apache.org>
2001-04-18 23:54:04 +0000

    Added concept of test run to allow various test suites to be more easily reused by different test processors. A test run captures the notion of run one or more test suites and processing their reports with a specific testReportProcessor.
    Improved HTML output for SVGRenderingAccuracyTest reports
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199742 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-sources/org/apache/batik/test/xml/XTRunConstants.java
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.test.xml;
+
+/**
+ * Contains constants for the XML Test Run (XTRun) syntax.
+ *
+ * @author <a href="mailto:vhardy@apache.org">Vincent Hardy</a>
+ * @version $Id$
+ */
+public interface XTRunConstants {
+    String XTRun_NAMESPACE_URI 
+        = "http://xml.apache.org/xml-batik/test/xtrun";
+
+    /////////////////////////////////////////////////////////////////////////
+    // XTRun tags
+    /////////////////////////////////////////////////////////////////////////
+    String XTRun_ARG_TAG = "arg";
+    String XTRun_PROPERTY_TAG = "property";
+    String XTRun_TEST_RUN_TAG = "testRun";
+    String XTRun_TEST_SUITE_TAG = "testSuite";
+    String XTRun_TEST_REPORT_PROCESSOR_TAG = "testReportProcessor";
+
+    /////////////////////////////////////////////////////////////////////////
+    // XTRun attributes
+    /////////////////////////////////////////////////////////////////////////
+    String XTRun_CLASS_ATTRIBUTE = "class";
+    String XTRun_HREF_ATTRIBUTE = "href";
+    String XTRun_NAME_ATTRIBUTE  = "name";
+    String XTRun_VALUE_ATTRIBUTE = "value";
+}
