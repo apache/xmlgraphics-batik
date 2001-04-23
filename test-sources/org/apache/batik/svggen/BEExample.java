ea083ee96773ba235ca8bdea2c11a1198dc4d178
Christophe Jolif <cjolif@apache.org>
2001-04-23 09:31:57 +0000

    translation of old regsvggen test files to new system (first part)
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199759 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-sources/org/apache/batik/svggen/BEExample.java
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.svggen;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.font.*;
+
+/**
+ * This test validates the convertion of Java 2D AffineTransform into SVG
+ * Shapes.
+ *
+ * @author <a href="mailto:cjolif@ilog.fr">Christophe Jolif</a>
+ * @author <a href="mailto:vhardy@eng.sun.com">Vincent Hardy</a>
+ * @version $Id$
+ */
+public class BEExample implements Painter {
+    public void paint(Graphics2D g) {
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                           RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Text
+        g.setPaint(new Color(103, 103, 152));
+        g.fillRect(10, 10, 200, 50);
+        g.setPaint(Color.white);
+        g.setFont(new Font("SunSansCondensed-Heavy", Font.PLAIN, 20));
+        g.drawString("Hello Java 2D to SVG", 40, 40);
+    }
+}
