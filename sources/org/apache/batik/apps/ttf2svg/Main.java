ee248766cabb69127ca12798b5518f06ccc5ef53
Vincent Hardy <vhardy@apache.org>
2001-04-27 18:22:35 +0000

    Initial empty-shell commit.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199785 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sources/org/apache/batik/apps/ttf2svg/Main.java
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.apps.ttf2svg;
+
+/**
+ * This test runs the True Type Font to SVG Font converter, the 
+ * tool that allows some characters from a font to be converted
+ * to the SVG Font format.
+ *
+ * @author <a href="mailto:vhardy@apache.org">Vincent Hardy</a>
+ * @version $Id$
+ */
+public class Main {
+}
+
