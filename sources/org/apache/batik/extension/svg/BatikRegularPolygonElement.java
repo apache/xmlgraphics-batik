27f02683d9ec2838b3af8d6279f4f54418289284
Thomas E. DeWeese <deweese@apache.org>
2001-05-15 13:58:46 +0000

    Missed these somehow first time around.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@199890 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sources/org/apache/batik/extension/svg/BatikRegularPolygonElement.java
@@ -0,0 +1,75 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.extension.svg;
+
+import org.apache.batik.dom.AbstractDocument;
+
+import org.w3c.dom.Node;
+import org.apache.batik.extension.StylableExtensionElement;
+
+/**
+ * This class implements a regular polygon extension to SVG
+ *
+ * @author <a href="mailto:thomas.deweese@kodak.com">Thomas DeWeese</a>
+ * @version $Id$
+ */
+public class BatikRegularPolygonElement
+    extends    StylableExtensionElement 
+    implements BatikExtConstants {
+
+    /**
+     * The element prefix.
+     */
+    protected String prefix = null;
+
+    /**
+     * Creates a new BatikRegularPolygonElement object.
+     */
+    protected BatikRegularPolygonElement() {
+    }
+
+    /**
+     * Creates a new BatikRegularPolygonElement object.
+     * @param prefix The namespace prefix.
+     * @param owner The owner document.
+     */
+    public BatikRegularPolygonElement(String prefix, AbstractDocument owner) {
+        super(prefix, owner);
+        this.prefix = null;
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link org.w3c.dom.Node#getNodeName()}.
+     */
+    public String getNodeName() {
+        return (prefix == null || prefix.equals(""))
+            ? getLocalName() : prefix + ":" + getLocalName();
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link org.w3c.dom.Node#getLocalName()}.
+     */
+    public String getLocalName() {
+        return BATIK_EXT_REGULAR_POLYGON_TAG;
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link org.w3c.dom.Node#getNamespaceURI()}.
+     */
+    public String getNamespaceURI() {
+        return BATIK_EXT_NAMESPACE_URI;
+    }
+
+    /**
+     * Returns a new uninitialized instance of this object's class.
+     */
+    protected Node newNode() {
+        return new BatikRegularPolygonElement();
+    }
+}
