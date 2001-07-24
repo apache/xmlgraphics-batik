bd86bd2055f632a35171ccc150726ebd51a24c7e
Stephane Hillion <hillion@apache.org>
2001-07-24 17:56:52 +0000

    feComposite is now stylable. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@200021 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/dom/svg/SVGOMFECompositeElement.java
+++ sources/org/apache/batik/dom/svg/SVGOMFECompositeElement.java
@@ -12,8 +12,17 @@ import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.batik.css.ElementNonCSSPresentationalHints;
+import org.apache.batik.css.ExtendedElementCSSInlineStyle;
 import org.apache.batik.dom.AbstractDocument;
+import org.apache.batik.dom.util.OverrideStyleElement;
+import org.apache.batik.dom.util.XLinkSupport;
+import org.apache.batik.dom.util.XMLSupport;
+
 import org.w3c.dom.Node;
+import org.w3c.dom.css.CSSStyleDeclaration;
+import org.w3c.dom.css.CSSValue;
+import org.w3c.dom.svg.SVGAnimatedBoolean;
 import org.w3c.dom.svg.SVGAnimatedEnumeration;
 import org.w3c.dom.svg.SVGAnimatedNumber;
 import org.w3c.dom.svg.SVGAnimatedString;
@@ -27,7 +36,10 @@ import org.w3c.dom.svg.SVGFECompositeElement;
  */
 public class SVGOMFECompositeElement
     extends    SVGOMFilterPrimitiveStandardAttributes
-    implements SVGFECompositeElement {
+    implements SVGFECompositeElement,
+               OverrideStyleElement,
+               ExtendedElementCSSInlineStyle,
+               ElementNonCSSPresentationalHints {
 
     /**
      * The DefaultAttributeValueProducer for operator.
@@ -282,6 +294,277 @@ public class SVGOMFECompositeElement
             k4Reference = new WeakReference(result);
         }
         return result;
+    }
+
+    // ElementNonCSSPresentationalHints ////////////////////////////////////
+
+    /**
+     * Returns the translation of the non-CSS hints to the corresponding
+     * CSS rules. The result can be null.
+     */
+    public CSSStyleDeclaration getNonCSSPresentationalHints() {
+        return ElementNonCSSPresentationalHintsSupport.
+            getNonCSSPresentationalHints(this);
+    }
+
+    // XLink support //////////////////////////////////////////////////////
+
+    /**
+     * The SVGURIReference support.
+     */
+    protected SVGURIReferenceSupport uriReferenceSupport;
+
+    /**
+     * Returns uriReferenceSupport different from null.
+     */
+    protected final SVGURIReferenceSupport getSVGURIReferenceSupport() {
+        if (uriReferenceSupport == null) {
+            uriReferenceSupport = new SVGURIReferenceSupport();
+        }
+        return uriReferenceSupport;
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#getHref()}.
+     */
+    public SVGAnimatedString getHref() {
+        return getSVGURIReferenceSupport().getHref(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#getXlinkType()}.
+     */
+    public String getXlinkType() {
+        return XLinkSupport.getXLinkType(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#setXlinkType(String)}.
+     */
+    public void setXlinkType(String str) {
+        XLinkSupport.setXLinkType(this, str);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#getXlinkRole()}.
+     */
+    public String getXlinkRole() {
+        return XLinkSupport.getXLinkRole(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#setXlinkRole(String)}.
+     */
+    public void setXlinkRole(String str) {
+        XLinkSupport.setXLinkRole(this, str);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#getXlinkArcRole()}.
+     */
+    public String getXlinkArcRole() {
+        return XLinkSupport.getXLinkArcRole(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#setXlinkArcRole(String)}.
+     */
+    public void setXlinkArcRole(String str) {
+        XLinkSupport.setXLinkArcRole(this, str);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#getXlinkTitle()}.
+     */
+    public String getXlinkTitle() {
+        return XLinkSupport.getXLinkTitle(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#setXlinkTitle(String)}.
+     */
+    public void setXlinkTitle(String str) {
+        XLinkSupport.setXLinkTitle(this, str);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#getXlinkShow()}.
+     */
+    public String getXlinkShow() {
+        return XLinkSupport.getXLinkShow(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#setXlinkShow(String)}.
+     */
+    public void setXlinkShow(String str) {
+        XLinkSupport.setXLinkShow(this, str);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#getXlinkActuate()}.
+     */
+    public String getXlinkActuate() {
+        return XLinkSupport.getXLinkActuate(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGURIReference#setXlinkActuate(String)}.
+     */
+    public void setXlinkActuate(String str) {
+        XLinkSupport.setXLinkActuate(this, str);
+    }
+
+    /**
+     * Returns the value of the 'xlink:href' attribute of the given element.
+     */
+    public String getXlinkHref() {
+        return XLinkSupport.getXLinkHref(this);
+    }
+
+    /**
+     * Sets the value of the 'xlink:href' attribute of the given element.
+     */
+    public void setXlinkHref(String str) {
+        XLinkSupport.setXLinkHref(this, str);
+    }
+
+    // SVGStylable support ///////////////////////////////////////////////////
+
+    /**
+     * The stylable support.
+     */
+    protected SVGStylableSupport stylableSupport;
+
+    /**
+     * Returns stylableSupport different from null.
+     */
+    protected final SVGStylableSupport getStylableSupport() {
+        if (stylableSupport == null) {
+            stylableSupport = new SVGStylableSupport();
+        }
+        return stylableSupport;
+    }
+
+    /**
+     * Implements {@link
+     * org.apache.batik.css.ExtendedElementCSSInlineStyle#hasStyle()}.
+     */
+    public boolean hasStyle() {
+        return SVGStylableSupport.hasStyle(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link org.w3c.dom.svg.SVGStylable#getStyle()}.
+     */
+    public CSSStyleDeclaration getStyle() {
+        return getStylableSupport().getStyle(this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGStylable#getPresentationAttribute(String)}.
+     */
+    public CSSValue getPresentationAttribute(String name) {
+        return getStylableSupport().getPresentationAttribute(name, this);
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * org.w3c.dom.svg.SVGStylable#getClassName()}.
+     */
+    public SVGAnimatedString getClassName() {
+        return getStylableSupport().getClassName(this);
+    }
+
+    // OverrideStyleElement ///////////////////////////////////////////
+
+    /**
+     * Implements {@link
+     * OverrideStyleElement#hasOverrideStyle(String)}.
+     */
+    public boolean hasOverrideStyle(String pseudoElt) {
+        return getStylableSupport().hasOverrideStyle(pseudoElt);
+    }
+
+    /**
+     * Implements {@link
+     * OverrideStyleElement#getOverrideStyle(String)}.
+     */
+    public CSSStyleDeclaration getOverrideStyle(String pseudoElt) {
+        return getStylableSupport().getOverrideStyle(pseudoElt, this);
+    }
+
+    // SVGExternalResourcesRequired support /////////////////////////////
+
+    /**
+     * The SVGExternalResourcesRequired support.
+     */
+    protected SVGExternalResourcesRequiredSupport
+        externalResourcesRequiredSupport;
+
+    /**
+     * Returns testsSupport different from null.
+     */
+    protected final SVGExternalResourcesRequiredSupport
+        getExternalResourcesRequiredSupport() {
+        if (externalResourcesRequiredSupport == null) {
+            externalResourcesRequiredSupport =
+                new SVGExternalResourcesRequiredSupport();
+        }
+        return externalResourcesRequiredSupport;
+    }
+
+    /**
+     * <b>DOM</b>: Implements {@link
+     * SVGExternalResourcesRequired#getExternalResourcesRequired()}.
+     */
+    public SVGAnimatedBoolean getExternalResourcesRequired() {
+        return getExternalResourcesRequiredSupport().
+            getExternalResourcesRequired(this);
+    }
+
+    // SVGLangSpace support //////////////////////////////////////////////////
+
+    /**
+     * <b>DOM</b>: Returns the xml:lang attribute value.
+     */
+    public String getXMLlang() {
+        return XMLSupport.getXMLLang(this);
+    }
+
+    /**
+     * <b>DOM</b>: Sets the xml:lang attribute value.
+     */
+    public void setXMLlang(String lang) {
+        XMLSupport.setXMLLang(this, lang);
+    }
+
+    /**
+     * <b>DOM</b>: Returns the xml:space attribute value.
+     */
+    public String getXMLspace() {
+        return XMLSupport.getXMLSpace(this);
+    }
+
+    /**
+     * <b>DOM</b>: Sets the xml:space attribute value.
+     */
+    public void setXMLspace(String space) {
+        XMLSupport.setXMLSpace(this, space);
     }
 
     /**
