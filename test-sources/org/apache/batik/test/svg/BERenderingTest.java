0407a74f8fbb6c05877328734207383afcecf275
Vincent Hardy <vhardy@apache.org>
2001-10-17 14:50:45 +0000

    New class with default directory conventions for BE tests. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@200297 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-sources/org/apache/batik/test/svg/BERenderingTest.java
@@ -0,0 +1,69 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.test.svg;
+
+/**
+ * Preconfigured test for SVG files under the xml-batik/../beSuite
+ * directory.
+ *
+ * @author <a href="vhardy@apache.org">Vincent Hardy</a>
+ * @version $Id$
+ */
+public class BERenderingTest extends PreconfiguredRenderingTest {
+    public static final String SVG_URL_PREFIX 
+        = "test-references/../../beSuite/";
+
+    public static final String REF_IMAGE_PREFIX 
+        = "test-references/svgbe/";
+
+    public static final String REF_IMAGE_SUFFIX
+        = "";
+
+    public static final String VARIATION_PREFIX
+        = "test-references/svgbe/";
+
+    public static final String VARIATION_SUFFIX
+        = "accepted-variation/";
+
+    public static final String SAVE_VARIATION_PREFIX
+        = "test-references/svgbe/";
+
+    public static final String SAVE_VARIATION_SUFFIX
+        = "candidate-variation/";
+
+    protected String getSVGURLPrefix(){
+        return SVG_URL_PREFIX;
+    }
+
+    protected String getRefImagePrefix(){
+        return REF_IMAGE_PREFIX;
+    }
+
+    protected String getRefImageSuffix(){
+        return REF_IMAGE_SUFFIX;
+    }
+
+    protected String getVariationPrefix(){
+        return VARIATION_PREFIX;
+    }
+
+    protected String getVariationSuffix(){
+        return VARIATION_SUFFIX;
+    }
+
+    protected String getSaveVariationPrefix(){
+        return SAVE_VARIATION_PREFIX;
+    }
+
+    protected String getSaveVariationSuffix(){
+        return SAVE_VARIATION_SUFFIX;
+    }
+
+
+}
