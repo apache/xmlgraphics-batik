380d6313dc2332bb8cead5f9537ed3fe1280d09d
Thierry Kormann <tkormann@apache.org>
2001-11-29 09:17:41 +0000

    change the layout
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@200505 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/util/gui/JErrorPane.java
+++ sources/org/apache/batik/util/gui/JErrorPane.java
@@ -13,6 +13,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
 import java.awt.Insets;
 
 import java.awt.event.ActionEvent;
@@ -24,15 +25,18 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -47,7 +51,7 @@ import org.apache.batik.util.gui.resource.ResourceManager;
  * @author <a href="mailto:tkormann@apache.org">Thierry Kormann</a>
  * @version $Id$
  */
-public class JErrorPane extends JOptionPane {
+public class JErrorPane extends JPanel implements ActionMap {
 
     /**
      * The resource file name
@@ -81,12 +85,43 @@ public class JErrorPane extends JOptionPane {
     protected String stacktrace;
 
     /**
+     * The button factory.
+     */
+    protected ButtonFactory bf = new ButtonFactory(bundle, this);
+
+    /**
+     * The text area used to show the stack trace.
+     */
+    protected JComponent detailsArea;
+
+    /**
+     * The button used to show or not the details.
+     */
+    protected JButton showDetailButton;
+
+    /**
+     * This flag bit indicates whether or not the stack trace is shown.
+     */
+    protected boolean isDetailShown = false;
+
+    /**
+     * The sub panel that contains the stack trace text area.
+     */
+    protected JPanel subpanel;
+
+    /**
      * Constructs a new JErrorPane.
      *
      * @param th the throwable object that describes the errror
      * @param type the dialog type
      */
     public JErrorPane(Throwable th, int type) {
+        super(new GridBagLayout());
+
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        listeners.put("ShowDetailButtonAction", new ShowDetailButtonAction());
+        listeners.put("OKButtonAction", new OKButtonAction());
         this.msg = th.getMessage();
 
         StringWriter writer = new StringWriter();
@@ -94,123 +129,112 @@ public class JErrorPane extends JOptionPane {
         writer.flush();
         this.stacktrace = writer.toString();
 
-        setMessage(new MessagePanel());
-        setMessageType(type);
+        ExtendedGridBagConstraints constraints =
+            new ExtendedGridBagConstraints();
+
+        JTextArea msgArea = new JTextArea();
+        msgArea.setText(msg);
+        msgArea.setColumns(50);
+        msgArea.setFont(new JLabel().getFont());
+        msgArea.setForeground(new JLabel().getForeground());
+        msgArea.setOpaque(false);
+        msgArea.setEditable(false);
+        msgArea.setLineWrap(true);
+
+        constraints.setWeight(0, 0);
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.setGridBounds(0, 0, 1, 1);
+        add(msgArea, constraints);
+
+        constraints.setWeight(1, 0);
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.setGridBounds(0, 1, 1, 1);
+        add(createButtonsPanel(), constraints);
+
+        JTextArea details = new JTextArea();
+        msgArea.setColumns(50);
+        details.setText(stacktrace);
+        details.setEditable(false);
+
+        detailsArea = new JPanel(new BorderLayout(0, 10));
+        detailsArea.add(new JSeparator(), BorderLayout.NORTH);
+        detailsArea.add(new JScrollPane(details), BorderLayout.CENTER);
+
+        subpanel = new JPanel(new BorderLayout());
+
+        constraints.insets = new Insets(10, 4, 4, 4);
+        constraints.setWeight(1, 1);
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.setGridBounds(0, 2, 1, 1);
+        add(subpanel, constraints);
+    }
+
+    public JDialog createDialog(Component owner, String title) {
+        JDialog dialog  =
+            new JDialog(JOptionPane.getFrameForComponent(owner), title);
+        dialog.getContentPane().add(this, BorderLayout.CENTER);
+        dialog.pack();
+        return dialog;
+    }
+
+    protected JPanel createButtonsPanel() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        showDetailButton = bf.createJButton("ShowDetailButton");
+        panel.add(showDetailButton);
+
+        JButton okButton = bf.createJButton("OKButton");
+        panel.add(okButton);
+
+        return panel;
     }
 
     /**
-     * The message panel.
+     * The map that contains the listeners
      */
-    protected class MessagePanel extends JPanel implements ActionMap {
+    protected Map listeners = new HashMap();
 
-        /**
-         * The button factory.
-         */
-        protected ButtonFactory bf = new ButtonFactory(bundle, this);
+    /**
+     * Returns the action associated with the given string or null on error
+     *
+     * @param key the key mapped with the action to get
+     * @throws MissingListenerException if the action is not found
+     */
+    public Action getAction(String key) throws MissingListenerException {
+        return (Action)listeners.get(key);
+    }
 
-        /**
-         * The text area used to show the stack trace.
-         */
-        protected JComponent detailsArea;
+    /**
+     * The action associated with the 'OK' button.
+     */
+    protected class OKButtonAction extends AbstractAction {
 
-        /**
-         * The button used to show or not the details.
-         */
-        protected JButton showDetailButton;
-
-        /**
-         * This flag bit indicates whether or not the stack trace is shown.
-         */
-        protected boolean isDetailShown = false;
-
-        /**
-         * The sub panel that contains the stack trace text area.
-         */
-        protected JPanel subpanel;
-
-        /**
-         * Constructs a new <tt>MessagePanel</tt>.
-         */
-        public MessagePanel() {
-            super(new GridBagLayout());
-
-            listeners.put("ShowDetailButtonAction",
-                          new ShowDetailButtonAction());
-
-            ExtendedGridBagConstraints constraints =
-                new ExtendedGridBagConstraints();
-
-            JTextArea msgArea = new JTextArea();
-            msgArea.setColumns(50);
-            msgArea.setFont(new JLabel().getFont());
-            msgArea.setForeground(new JLabel().getForeground());
-            msgArea.setOpaque(false);
-            msgArea.setEditable(false);
-            msgArea.setText(msg);
-            msgArea.setLineWrap(true);
-
-            constraints.insets = new Insets(5, 5, 5, 5);
-            constraints.setWeight(0, 0);
-            constraints.anchor = GridBagConstraints.WEST;
-            constraints.fill = GridBagConstraints.NONE;
-            constraints.setGridBounds(0, 0, 1, 1);
-            add(msgArea, constraints);
-
-            constraints.insets = new Insets(0, 5, 5, 5);
-            showDetailButton = bf.createJButton("ShowDetailButton");
-            constraints.setWeight(0, 0);
-            constraints.anchor = GridBagConstraints.NORTHWEST;
-            constraints.fill = GridBagConstraints.NONE;
-            constraints.setGridBounds(0, 1, 1, 1);
-            add(showDetailButton, constraints);
-
-            JTextArea details = new JTextArea();
-            details.setText(stacktrace);
-            details.setEditable(false);
-            detailsArea = new JScrollPane(details);
-
-            subpanel = new JPanel(new BorderLayout());
-            constraints.setWeight(1d, 1d);
-            constraints.anchor = GridBagConstraints.CENTER;
-            constraints.fill = GridBagConstraints.BOTH;
-            constraints.setGridBounds(0, 2, 1, 1);
-            add(subpanel, constraints);
+        public void actionPerformed(ActionEvent evt) {
+            ((JDialog)getTopLevelAncestor()).dispose();
         }
+    }
 
-        /**
-         * The map that contains the listeners
-         */
-        protected Map listeners = new HashMap();
+    /**
+     * The action associated with the 'Show Detail' button.
+     */
+    protected class ShowDetailButtonAction extends AbstractAction {
 
-        /**
-         * Returns the action associated with the given string or null on error
-         *
-         * @param key the key mapped with the action to get
-         * @throws MissingListenerException if the action is not found
-         */
-        public Action getAction(String key) throws MissingListenerException {
-            return (Action)listeners.get(key);
-        }
-
-        /**
-         * The action associated with the 'Show Detail' button.
-         */
-        protected class ShowDetailButtonAction extends AbstractAction {
-
-            public void actionPerformed(ActionEvent evt) {
-                if (isDetailShown) {
-                    subpanel.remove(detailsArea);
-                    isDetailShown = false;
-                    showDetailButton.setText
-                        (resources.getString("ShowDetailButton.text"));
-                } else {
-                    subpanel.add(detailsArea, BorderLayout.CENTER);
-                    showDetailButton.setText
-                        (resources.getString("ShowDetailButton.text2"));
-                    isDetailShown = true;
-                }
-                ((JDialog)getTopLevelAncestor()).pack();
+        public void actionPerformed(ActionEvent evt) {
+            if (isDetailShown) {
+                subpanel.remove(detailsArea);
+                isDetailShown = false;
+                showDetailButton.setText
+                    (resources.getString("ShowDetailButton.text"));
+            } else {
+                subpanel.add(detailsArea, BorderLayout.CENTER);
+                showDetailButton.setText
+                    (resources.getString("ShowDetailButton.text2"));
+                isDetailShown = true;
             }
+            ((JDialog)getTopLevelAncestor()).pack();
         }
     }
 }
