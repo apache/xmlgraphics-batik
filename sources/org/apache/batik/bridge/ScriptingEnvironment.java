5a38c90c219f12367c19d2b430b54ba309fb3aa3
Stephane Hillion <hillion@apache.org>
2002-02-12 07:08:17 +0000

    'setTimeout' now works with optional parameters.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@200618 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/bridge/ScriptingEnvironment.java
+++ sources/org/apache/batik/bridge/ScriptingEnvironment.java
@@ -16,6 +16,7 @@ import java.util.List;
 import org.apache.batik.script.Interpreter;
 import org.apache.batik.script.InterpreterException;
 import org.apache.batik.script.InterpreterPool;
+import org.apache.batik.script.JavaFunction;
 
 import org.apache.batik.util.Lock;
 import org.apache.batik.util.RunnableQueue;
@@ -32,6 +33,7 @@ import org.w3c.dom.events.Event;
 public class ScriptingEnvironment {
 
     private final static String EVENT_NAME = "evt";
+    private final static String ARG_NAME = "arg__";
 
     /**
      * The scripting lock.
@@ -54,11 +56,6 @@ public class ScriptingEnvironment {
     protected RunnableQueue updateRunnableQueue;
 
     /**
-     * The active scripting threads.
-     */
-    protected List scripts = Collections.synchronizedList(new LinkedList());
-
-    /**
      * Whether the scripts must be suspended.
      */
     protected volatile boolean suspended;
@@ -67,6 +64,11 @@ public class ScriptingEnvironment {
      * The suspend lock.
      */
     protected Object suspendLock = new Object();
+
+    /**
+     * The alert function.
+     */
+    protected JavaFunction alertFunction;
 
     /**
      * Creates a new ScriptingEnvironment.
@@ -80,10 +82,60 @@ public class ScriptingEnvironment {
     }
 
     /**
-     * Runs a script.
+     * Sets the environment of the given interpreter.
      */
-    public void runScript(String script, String lang, Event evt) {
-        new ScriptingThread(script, lang, evt).start();
+    public void setEnvironment(Interpreter interp, String lang) {
+        interp.bindObject("alert", createAlertFunction());
+        interp.bindObject("setTimeout", createSetTimeoutFunction(lang));
+    }
+
+    private JavaFunction createAlertFunction() {
+        if (alertFunction == null) {
+            alertFunction = new JavaFunction() {
+                    public Class[] getParameterTypes() {
+                        return new Class[] { String.class };
+                    }
+                    public Object call(Object[] arguments) {
+                        javax.swing.JOptionPane.showMessageDialog
+                            (null, (String)arguments[0]);
+                        return null;
+                    }
+                };
+        }
+        return alertFunction;
+    }
+
+
+    private final static Class[] parameters = { String.class, Long.TYPE };
+    private JavaFunction createSetTimeoutFunction(final String lang) {
+        return new JavaFunction() {
+                public Class[] getParameterTypes() {
+                    return parameters;
+                }
+                public Object call(Object[] args) {
+                    pauseScript(((Long)args[1]).longValue());
+                    Object[] fargs = new Object[args.length - 2];
+                    for (int i = 0; i < fargs.length; i++) {
+                        fargs[i] = args[i + 2];
+                    }
+                    runFunction((String)args[0], fargs, lang);
+                    return null;
+                }
+            };
+    }
+
+    /**
+     * Runs an event handler.
+     */
+    public void runEventHandler(String script, Event evt, String lang) {
+        new EventHandlerThread(script, evt, lang).start();
+    }
+
+    /**
+     * Runs a function.
+     */
+    public void runFunction(String function, Object[] args, String lang) {
+        new FunctionCallThread(function, args, lang).start();
     }
 
     /**
@@ -170,51 +222,125 @@ public class ScriptingEnvironment {
     }
 
     /**
-     * To run a script.
+     * To run a piece of script.
      */
-    protected class ScriptingThread extends Thread {
-        protected String script;
-        protected Event event;
+    protected abstract class ScriptingThread extends Thread {
         protected UserAgent userAgent;
         protected Interpreter interpreter;
-        public ScriptingThread(String script, String lang, Event evt) {
-            this.script = script;
-            event = evt;
+
+        /**
+         * Creates a new scripting thread.
+         */
+        public ScriptingThread(String lang) {
             BridgeContext bc = updateManager.getBridgeContext();
             userAgent = bc.getUserAgent();
             Document doc = updateManager.getDocument();
             interpreter = bc.getInterpreterPool().getInterpreter(doc, lang);
-            
             if (interpreter == null) {
                 if (userAgent != null) {
-                    userAgent.displayError(new Exception("unknow language: "+
-                                                         lang));
+                    userAgent.displayError
+                        (new Exception("unknow language: " + lang));
                 }
             }
         }
+
+        /**
+         * The main method.
+         */
         public void run() {
             if (interpreter != null) {
                 beginScript();
-                scripts.add(this);
-            
-                if (event != null) {
-                    interpreter.bindObject(EVENT_NAME, event);
-                }
+
                 try {
-                    interpreter.evaluate(script);
+                    interpreter.evaluate(getScript());
                 } catch (InterpreterException ie) {
                     Exception ex = ie.getException();
                     if (ex instanceof StopScriptException) {
-                        scripts.remove(this);
                         return;
                     }
                     if (userAgent != null) {
                         userAgent.displayError((ex != null) ? ex : ie);
                     }
+                } finally {
+                    try {
+                        endScript();
+                    } catch (StopScriptException e) {
+                    }
                 }
-                scripts.remove(this);
-                endScript();
             }
+        }
+
+        /**
+         * Returns the script to execute.
+         */
+        protected abstract String getScript();
+
+    }
+
+    /**
+     * To run a function.
+     */
+    protected class FunctionCallThread extends ScriptingThread {
+        protected String function;
+        protected Object[] arguments;
+
+        /**
+         * Creates a new FunctionCallThread.
+         */
+        public FunctionCallThread(String fname, Object[] args, String lang) {
+            super(lang);
+            function = fname;
+            arguments = args;
+        }
+
+        /**
+         * Returns the script to execute.
+         */
+        protected String getScript() {
+            if (function.endsWith("()")) {
+                function = function.substring(0, function.length() - 2);
+            }
+            StringBuffer sb = new StringBuffer(function);
+
+            sb.append("(");
+            if (arguments.length > 0) {
+                String s = ARG_NAME + 0;
+                sb.append(s);
+                interpreter.bindObject(s, arguments[0]);
+                for (int i = 1; i < arguments.length; i++) {
+                    s = ARG_NAME + i;
+                    sb.append(",");
+                    sb.append(s);
+                    interpreter.bindObject(s, arguments[i]);
+                }
+            }
+            sb.append(")");
+            return sb.toString();
+        }
+    }
+
+    /**
+     * To run an event handler.
+     */
+    protected class EventHandlerThread extends ScriptingThread {
+        protected String script;
+        protected Event event;
+
+        /**
+         * Creates a new EventHandlerThread.
+         */
+        public EventHandlerThread(String script, Event evt, String lang) {
+            super(lang);
+            this.script = script;
+            event = evt;
+        }
+
+        /**
+         * Returns the script to execute.
+         */
+        protected String getScript() {
+            interpreter.bindObject(EVENT_NAME, event);
+            return script;
         }
     }
 
