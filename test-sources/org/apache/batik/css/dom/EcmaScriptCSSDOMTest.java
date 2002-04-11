3234ec95ff77e89e4413a90e463c72db3a47e38a
Vincent Hardy <vhardy@apache.org>
2002-04-11 08:07:24 +0000

    New test which allows tests, written in ECMAScript, to be integrated in our test infrastructure. Tests need to run in response to the document's 'onload' event and report their result in a <testResult> element (and children <errorDescriptionEntry>) metadata. That metadata should be in the name space 'http://xml.apache.org/batik/test' to be usable by the infrastructure. See test-resources/org/apache/batik/css/dom/rgbTest.svg for an example (and related test-resources/org/apache/batik/css/dom/unitTesting.xml file)
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@200776 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-sources/org/apache/batik/css/dom/EcmaScriptCSSDOMTest.java
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.css.dom;
+
+import org.apache.batik.test.svg.SelfContainedSVGOnLoadTest;
+
+/**
+ * Helper class to simplify writing the unitTesting.xml file for 
+ * CSS DOM Tests.
+ *
+ * @author <a href="mailto:vhardy@apache.org">Vincent Hardy</a>
+ * @version $Id$
+ */
+
+public class EcmaScriptCSSDOMTest extends SelfContainedSVGOnLoadTest {
+    public void setId(String id){
+        super.setId(id);
+        svgURL = resolveURL("test-resources/org/apache/batik/css/dom/" + id + ".svg");
+    }
+}
