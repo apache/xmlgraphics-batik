c9fc5bc9cda64351b1b04678427a03b79f0be1d4
Vincent Hardy <vhardy@apache.org>
2002-05-02 15:46:26 +0000

    Created utility to let class easily enforce script security. This is now used by the browser and the rasterizer
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@200832 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/apps/svgbrowser/Main.java
+++ sources/org/apache/batik/apps/svgbrowser/Main.java
@@ -51,6 +51,7 @@ import org.apache.batik.util.PreferenceManager;
 import org.apache.batik.util.XMLResourceDescriptor;
 import org.apache.batik.util.SVGConstants;
 
+import org.apache.batik.util.ApplicationSecurityEnforcer;
 import org.apache.batik.util.gui.resource.ResourceManager;
 
 /**
@@ -83,81 +84,16 @@ public class Main implements Application {
         "org.apache.batik.apps.svgbrowser.resources.Main";
 
     /**
-     * Message for the SecurityException thrown when there is already
-     * a SecurityManager installed at the time Squiggle tries
-     * to install its own security settings.
+     * Name of the batik browser jar file
      */
-    public static final String EXCEPTION_ALIEN_SECURITY_MANAGER
-        = Resources.getString("Main.message.security.exception.alien.security.manager");
-
-    /**
-     * Message for the NullPointerException thrown when no policy
-     * file can be found.
-     */
-    public static final String EXCEPTION_NO_SQUIGGLE_POLICY_FILE
-        = Resources.getString("Main.message.null.pointer.exception.no.squiggle.policy.file");
-
-    /**
-     * System property for specifying an additional policy file.
-     */
-    public static final String PROPERTY_JAVA_SECURITY_POLICY 
-        = "java.security.policy";
-
-    /**
-     * System property for Squiggle's development base directory
-     */
-    public static final String PROPERTY_SQUIGGLE_DEV_BASE
-        = "squiggle.dev.base";
-
-    /**
-     * System property for Squiggle's jars base directory
-     */
-    public static final String PROPERTY_SQUIGGLE_JAR_BASE
-        = "squiggle.jar.base";
+    public static final String SQUIGGLE_JAR_NAME
+        = "batik-svgbrowser.jar";
 
     /**
      * URL for Squiggle's security policy file
      */
-    public static final String SQUIGGLE_SECURITY_POLICY_URL
-        = "org/apache/batik/apps/svgbrowser/resources/svgbrowser.policy"; // "resources/org/apache/batik/apps/svgbrowser/resources/svgbrowser.policy";
-
-    /**
-     * URL for Squiggle's main class.
-     */
-    public static final String SQUIGGLE_MAIN_CLASS_URL
-        = "org/apache/batik/apps/svgbrowser/Main.class"; 
-
-    /**
-     * Used in jar file urls to separate the jar file name 
-     * from the referenced file
-     */
-    public static final String JAR_URL_FILE_SEPARATOR
-        = "!/";
-
-    /**
-     * Files in a jar file have a URL with the jar protocol
-     */
-    public static final String JAR_PROTOCOL
-        = "jar:";
-
-    /**
-     * Directory where classes are expanded in the development
-     * version
-     */
-    public static final String SQUIGGLE_MAIN_CLASS_DIR
-        = "classes/";
-
-    /**
-     * Name of the batik browser jar file
-     */
-    public static final String SQUIGGLE_ENTRY_JAR_NAME
-        = "batik-svgbrowser.jar";
-
-    /**
-     * Resource directory for the security policy
-     */
-    public static final String SQUIGGLE_RESOURCE_DIR 
-        = "resources/";
+    public static final String SQUIGGLE_SECURITY_POLICY
+        = "org/apache/batik/apps/svgbrowser/resources/svgbrowser.policy"; 
 
     /**
      * The resource bundle
@@ -190,10 +126,10 @@ public class Main implements Application {
     protected String[] arguments;
 
     /**
-     * Controls whether the initial SecurityManager check
-     * has been done.
+     * Script security enforcement is delegated to the 
+     * security utility 
      */
-    protected boolean initialSecurityCheckDone;
+    protected ApplicationSecurityEnforcer securityEnforcer;
 
     /**
      * The option handlers.
@@ -220,6 +156,7 @@ public class Main implements Application {
     public Main(String[] args) {
         arguments = args;
 
+        //
         // Preferences
         //
         Map defaults = new HashMap(11);
@@ -253,6 +190,11 @@ public class Main implements Application {
         defaults.put(PreferenceDialog.PREFERENCE_KEY_CONSTRAIN_SCRIPT_ORIGIN,
                      Boolean.TRUE);
 	
+        securityEnforcer 
+            = new ApplicationSecurityEnforcer(this.getClass(),
+                                              SQUIGGLE_SECURITY_POLICY,
+                                              SQUIGGLE_JAR_NAME);
+                                              
         try {
             preferenceManager = new XMLPreferenceManager("preferences.xml",
                                                          defaults);
@@ -263,8 +205,10 @@ public class Main implements Application {
             preferenceManager.load();
             setPreferences();
         } catch (Exception e) {
+            e.printStackTrace();
         }
 
+        //
         // Initialization
         //
         final AboutDialog initDialog = new AboutDialog();
@@ -306,6 +250,9 @@ public class Main implements Application {
         });
         c.setSize(100, 100);
         c.loadSVGDocument(Main.class.getResource("resources/init.svg").toString());
+
+                                              
+                                                           
     }
 
     /**
@@ -552,150 +499,10 @@ public class Main implements Application {
         System.setProperty("proxyPort", preferenceManager.getString
                            (PreferenceDialog.PREFERENCE_KEY_PROXY_PORT));
 
-        if (!initialSecurityCheckDone){
-            setInitialSecurityPreferences();
-        } else {
-            setSecurityPreferences();
-        }
-    }
-
-    private void setSecurityPreferences() {
-        boolean enforceSecureScripting 
-            = preferenceManager.getBoolean(PreferenceDialog.PREFERENCE_KEY_ENFORCE_SECURE_SCRIPTING);
-
-        if (enforceSecureScripting) {
-            //
-            // Check if there is an already installed SecurityManager
-            //
-            SecurityManager currentManager = System.getSecurityManager();
-            if (currentManager == null){
-                installSecurityManager();
-            }
-        } else {
-            System.setSecurityManager(null);
-        }
-    }
-
-    private void setInitialSecurityPreferences(){
-        boolean enforceSecureScripting 
-            = preferenceManager.getBoolean(PreferenceDialog.PREFERENCE_KEY_ENFORCE_SECURE_SCRIPTING);
-
-        if (enforceSecureScripting) {
-            //
-            // Check if there is an already installed SecurityManager
-            //
-            SecurityManager currentManager = System.getSecurityManager();
-            if (currentManager != null){
-                //
-                // It would be unsafe to continue if there is already a SecurityManager 
-                // in place. This means Squiggle is not running as it was expected to.
-                // Therefore, we throw a SecurityException
-                //
-                throw new SecurityException(EXCEPTION_ALIEN_SECURITY_MANAGER);
-            } else {
-                initialSecurityCheckDone = true;
-            }
-
-            installSecurityManager();
-        }
-    }
-
-    private void installSecurityManager(){
-        // Make sure the security policy is enforced.
-        Policy policy = Policy.getPolicy();
-
-        // Install a new SecurityManager        
-        SecurityManager securityManager = new SecurityManager();
-
-        // Specify squiggle's security policy in the
-        // system property. 
-
-        ClassLoader cl = this.getClass().getClassLoader();
-        URL url = cl.getResource(SQUIGGLE_SECURITY_POLICY_URL);
-
-        if (url == null) {
-            throw new NullPointerException(EXCEPTION_NO_SQUIGGLE_POLICY_FILE);
-        }
-
-        System.setProperty(PROPERTY_JAVA_SECURITY_POLICY,
-                           url.toString());
-
-        // 
-        // The following detects whether the browser is running in the
-        // development environment, in which case it will set the 
-        // squiggle.dev.base property or if it is running in the binary
-        // distribution, in which case it will set the squiggle.jar.base
-        // property. These properties are expanded in the security 
-        // policy files.
-        // Property expansion is used to provide portability of the 
-        // policy files between various code bases (e.g., file base,
-        // server base, etc..).
-        //
-        url = cl.getResource(SQUIGGLE_MAIN_CLASS_URL);
-        if (url == null){
-            // Something is really wrong: we would be running a class
-            // which can't be found....
-            throw new Error();
-        }
-        
-        String expandedMainClassName = url.toString();
-        if (expandedMainClassName.indexOf(SQUIGGLE_ENTRY_JAR_NAME) != -1) {
-            setSquiggleJarBase(expandedMainClassName);
-        } else {
-            setSquiggleDevBase(expandedMainClassName);
-        }
-            
-        // Install new security manager
-        System.setSecurityManager(securityManager);
-
-        System.out.flush();
-        policy.refresh();
-
-    }
-
-    private void setSquiggleJarBase(String expandedMainClassName){
-        if (!expandedMainClassName.startsWith(JAR_PROTOCOL)){
-            // Something is seriously wrong here
-            throw new Error();
-        }
-
-        expandedMainClassName = expandedMainClassName.substring(JAR_PROTOCOL.length());
-
-        int codeBaseEnd = 
-            expandedMainClassName.indexOf(SQUIGGLE_ENTRY_JAR_NAME +
-                                      JAR_URL_FILE_SEPARATOR +
-                                      SQUIGGLE_MAIN_CLASS_URL);
-
-        if (codeBaseEnd == -1){
-            // Something is seriously wrong. This should *never* happen
-            // as the SQUIGGLE_SECURITY_POLICY_URL is such that it will be
-            // a substring of its corresponding URL value
-            throw new Error();
-        }
-
-        String squiggleCodeBase = expandedMainClassName.substring(0, codeBaseEnd);
-        System.setProperty(PROPERTY_SQUIGGLE_JAR_BASE, squiggleCodeBase);
-    }
-
-    /**
-     * Position the squiggle.dev.base property for expansion in 
-     * the policy file used when Squiggle is running in its 
-     * development version
-     */
-    private void setSquiggleDevBase(String expandedMainClassName){
-        int codeBaseEnd = 
-            expandedMainClassName.indexOf(SQUIGGLE_MAIN_CLASS_DIR + 
-                                          SQUIGGLE_MAIN_CLASS_URL);
-
-        if (codeBaseEnd == -1){
-            // Something is seriously wrong. This should *never* happen
-            // as the SQUIGGLE_SECURITY_POLICY_URL is such that it will be
-            // a substring of its corresponding URL value
-            throw new Error();
-        }
-
-        String squiggleCodeBase = expandedMainClassName.substring(0, codeBaseEnd);
-        System.setProperty(PROPERTY_SQUIGGLE_DEV_BASE, squiggleCodeBase);
+        securityEnforcer.enforceSecurity
+            (preferenceManager.getBoolean
+             (PreferenceDialog.PREFERENCE_KEY_ENFORCE_SECURE_SCRIPTING)
+             );
     }
 
     private void setPreferences(JSVGViewerFrame vf) {
---
+++ sources/org/apache/batik/util/Messages.java
@@ -0,0 +1,86 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.util;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import org.apache.batik.i18n.Localizable;
+import org.apache.batik.i18n.LocalizableSupport;
+import org.apache.batik.util.gui.resource.ResourceManager;
+
+/**
+ * This class manages the message for the security utilities
+ *
+ * @author <a href="mailto:vhardy@apache.org">Vincent Hardy</a>
+ * @version $Id$
+ */
+public class Messages {
+
+    /**
+     * This class does not need to be instantiated.
+     */
+    protected Messages() { }
+
+    /**
+     * The error messages bundle class name.
+     */
+    protected final static String RESOURCES =
+        "org.apache.batik.util.security.resources.Messages";
+
+    /**
+     * The localizable support for the error messages.
+     */
+    protected static LocalizableSupport localizableSupport =
+        new LocalizableSupport(RESOURCES);
+
+    /**
+     * The resource manager to decode messages.
+     */
+    protected static ResourceManager resourceManager =
+        new ResourceManager(localizableSupport.getResourceBundle());
+
+    /**
+     * Implements {@link org.apache.batik.i18n.Localizable#setLocale(Locale)}.
+     */
+    public static void setLocale(Locale l) {
+        localizableSupport.setLocale(l);
+        resourceManager = new ResourceManager(localizableSupport.getResourceBundle());
+    }
+
+    /**
+     * Implements {@link org.apache.batik.i18n.Localizable#getLocale()}.
+     */
+    public static Locale getLocale() {
+        return localizableSupport.getLocale();
+    }
+
+    /**
+     * Implements {@link
+     * org.apache.batik.i18n.Localizable#formatMessage(String,Object[])}.
+     */
+    public static String formatMessage(String key, Object[] args)
+        throws MissingResourceException {
+        return localizableSupport.formatMessage(key, args);
+    }
+
+    public static String getString(String key)
+        throws MissingResourceException {
+        return resourceManager.getString(key);
+    }
+
+    public static int getInteger(String key) 
+        throws MissingResourceException {
+        return resourceManager.getInteger(key);
+    }
+
+    public static int getCharacter(String key)
+        throws MissingResourceException {
+        return resourceManager.getCharacter(key);
+    }
+}
