6101f8e1afbdbb8e2c8fbaf17e92b0fb5778d446
Vincent Hardy <vhardy@apache.org>
2002-06-06 08:21:22 +0000

    Added test showing bug on eval. Fix is following
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@200906 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-sources/org/apache/batik/script/rhino/ScriptSelfTest.java
@@ -0,0 +1,106 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.batik.script.rhino;
+
+import org.apache.batik.test.*;
+import org.apache.batik.util.ApplicationSecurityEnforcer;
+import org.apache.batik.util.ParsedURL;
+import org.apache.batik.test.svg.SelfContainedSVGOnLoadTest;
+import org.apache.batik.bridge.UserAgent;
+import org.apache.batik.bridge.UserAgentAdapter;
+import org.apache.batik.bridge.ScriptSecurity;
+import org.apache.batik.bridge.DefaultScriptSecurity;
+import org.apache.batik.bridge.NoLoadScriptSecurity;
+import org.apache.batik.bridge.RelaxedScriptSecurity;
+
+/**
+ * Helper class to simplify writing the unitTesting.xml file for 
+ * scripting. The "id" for the test needs to be the path of the 
+ * selft contained SVG test, starting from:
+ * <xml-batik-dir>/test-resources/org/apache/batik/
+ *
+ * @author <a href="mailto:vhardy@apache.org">Vincent Hardy</a>
+ * @version $Id$
+ */
+
+public class ScriptSelfTest extends SelfContainedSVGOnLoadTest {
+    boolean secure = true;
+    boolean constrain = true;
+    String scripts = "text/ecmascript, application/java-archive";
+    TestUserAgent userAgent = new TestUserAgent();
+
+    public void setId(String id){
+        super.setId(id);
+        svgURL = resolveURL("test-resources/org/apache/batik/" + id + ".svg");
+    }
+
+    public void setSecure(Boolean secure){
+        this.secure = secure.booleanValue();
+    }
+
+    public Boolean getSecure(){
+        return new Boolean(this.secure);
+    }
+
+    public void setConstrain(Boolean constrain){
+        this.constrain = constrain.booleanValue();
+    }
+
+    public Boolean getConstrain(){
+        return new Boolean(this.constrain);
+    }
+
+    public void setScripts(String scripts){
+        this.scripts = scripts;
+    }
+
+    public String getScripts(){
+        return scripts;
+    }
+
+    public TestReport runImpl() throws Exception{
+        ApplicationSecurityEnforcer ase
+            = new ApplicationSecurityEnforcer(this.getClass(),
+                                              "org/apache/batik/apps/svgbrowser/resources/svgbrowser.policy",
+                                              "dummy.jar");
+
+        if (secure) {
+            ase.enforceSecurity(true);
+        }
+
+        try {
+            return super.runImpl();
+        } finally {
+            ase.enforceSecurity(false);
+        }
+    }
+
+    protected UserAgent buildUserAgent(){
+        return userAgent;
+    }
+    
+    class TestUserAgent extends UserAgentAdapter {
+        public ScriptSecurity getScriptSecurity(String scriptType,
+                                                ParsedURL scriptPURL,
+                                                ParsedURL docPURL){
+            if (scripts.indexOf(scriptType) == -1){
+                return new NoLoadScriptSecurity(scriptType);
+            } else {
+                if (constrain){
+                    return new DefaultScriptSecurity
+                        (scriptType, scriptPURL, docPURL);
+                } else {
+                    return new RelaxedScriptSecurity
+                        (scriptType, scriptPURL, docPURL);
+                }
+            }
+        }
+    }
+
+}
