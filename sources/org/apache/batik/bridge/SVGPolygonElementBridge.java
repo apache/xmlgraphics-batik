a1881565dc7969a0bf655a73b077dd4555fce526
Thierry Kormann <tkormann@apache.org>
2002-09-05 06:47:50 +0000

    fix bug #10122
    'd', and 'points' attributes can now be empty on <path>, <polyline>
    and <polygon>.
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@201031 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/bridge/SVGPolygonElementBridge.java
+++ sources/org/apache/batik/bridge/SVGPolygonElementBridge.java
@@ -75,9 +75,6 @@ public class SVGPolygonElementBridge extends SVGDecoratedShapeElementBridge {
             } finally {
                 shapeNode.setShape(app.getShape());
             }
-        } else {
-            throw new BridgeException(e, ERR_ATTRIBUTE_MISSING,
-                                      new Object[] {SVG_POINTS_ATTRIBUTE});
         }
     }
 
