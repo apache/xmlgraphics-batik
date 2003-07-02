d3d884dbdde94ca2d4658d925eaf58789bc40b50
Vincent Hardy <vhardy@apache.org>
2003-07-02 08:18:07 +0000

    Modified build.bat so that it now works if ANT is already installed. Use the ant.bat file which should be in the PATH if ant was installed
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@201191 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -14,7 +14,6 @@ goto cleanup
 
 if not "%ANT_HOME%" == "" goto gotAntHome
 set ANT_HOME=.
-:gotAntHome
 
 :: ----- Set Up The Runtime Classpath -----------------------------------------
 
@@ -27,6 +26,14 @@ set CP=%JAVA_HOME%\lib\tools.jar;%ANT_HOME%\lib\build\ant_1_4_1.jar;.\lib\build\
 %JAVA_HOME%\bin\java.exe %ANT_OPTS% -classpath %CP% org.apache.tools.ant.Main -emacs -Dant.home=%ANT_HOME% %1 -Dargs="%2 %3 %4 %5 %6 %7 %8 %9"
 
 :: ----- Cleanup the environment ----------------------------------------------
+
+goto cleanup
+
+:gotAntHome
+
+call ant -Dargs="%2 %3 %4 %5 %6 %7 %8 %9" %1
+
+goto cleanup
 
 :cleanup
 set CP=
