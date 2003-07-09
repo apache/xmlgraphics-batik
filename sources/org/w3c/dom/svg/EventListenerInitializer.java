095d5f685b6d8bc14e8b16160ce7ba74471fc503
Christophe Jolif <cjolif@apache.org>
2003-07-09 13:00:46 +0000

    add/modify classes for Batik to be conformant with SVG 1.1 specification Appendix D, on Java language binding. This is closing BR #21374
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@201228 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/w3c/dom/svg/EventListenerInitializer.java
+++ sources/org/w3c/dom/svg/EventListenerInitializer.java
@@ -6,33 +6,30 @@
  * the LICENSE file.                                                         *
  *****************************************************************************/
 
-package org.apache.batik.script;
-
-import org.w3c.dom.Document;
+package org.w3c.dom.svg;
 
 /**
  * This interface must be implemented in order to call Java code from
  * an SVG document.
  *
- * A ScriptHandler instance is called when a 'script' element's 'type'
- * attribute value is 'application/java-archive' and when the
- * manifest of the jar file referenced by the 'xlink:href' attribute
- * contains a 'Script-Handler' entry.  The value of this entry must be
- * the classname of the ScriptHandler to call.
+ * A <code>EventListenerInitializer</code> instance is called when
+ * a 'script' element's 'type' attribute value is 'application/java-archive' and when
+ * the manifest of the jar file referenced by the 'xlink:href' attribute contains
+ * a 'SVG-Handler-Class' entry.  The value of this entry must be the classname of the
+ * <code>EventListenerInitializer</code> to call.
  *
  * This classes implementing this interface must have a default
  * constructor.
  *
- * @author <a href="mailto:stephane@hillion.org">Stephane Hillion</a>
  * @version $Id$
  */
-public interface ScriptHandler {
+public interface EventListenerInitializer {
 
     /**
-     * Runs this handler.  This method is called by the SVG viewer
-     * when the scripts are loaded.
+     * This method is called by the SVG viewer
+     * when the scripts are loaded to register
+     * the listener needed.
      * @param doc The current document.
-     * @param win An object which represents the current viewer.
      */
-    void run(Document doc, Window win);
+    public void initializeEventListeners(SVGDocument doc);
 }
