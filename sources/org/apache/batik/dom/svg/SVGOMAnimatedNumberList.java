8c354f3a149e123ccb375ca8d64974d1eaaeaae9
Thomas E. DeWeese <deweese@apache.org>
2003-10-29 00:28:00 +0000

    1) Added Tonny Kohar's contribution to Batik - Thanks Tonny! 2) Added JSVG a very simple SVG viewer to the contrib area.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@201313 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sources/org/apache/batik/dom/svg/SVGOMAnimatedNumberList.java
@@ -0,0 +1,218 @@
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Batik" and  "Apache Software Foundation" must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation. For more  information on the
+ Apache Software Foundation, please see <http://www.apache.org/>.
+
+*/
+
+package org.apache.batik.dom.svg;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGAnimatedNumberList;
+import org.w3c.dom.svg.SVGException;
+import org.w3c.dom.svg.SVGNumberList;
+
+/**
+ * This class is the implementation of
+ * the SVGAnimatedNumberList interface.
+ *
+ * @author  tonny@kiyut.com
+ */
+public class SVGOMAnimatedNumberList 
+    implements SVGAnimatedNumberList,
+               LiveAttributeValue {
+    
+    /**
+     * The associated element.
+     */
+    protected AbstractElement element;
+
+    /**
+     * The attribute's namespace URI.
+     */
+    protected String namespaceURI;
+
+    /**
+     * The attribute's local name.
+     */
+    protected String localName;
+
+    /**
+     * Whether the list is changing.
+     */
+    protected boolean changing;
+
+    /**
+     * SVGNumberList
+     */
+    protected AbstractSVGNumberList numbers;
+
+    /**
+     * Default value for the 'points' attribute.
+     */
+    protected String defaultValue;
+    
+    /**
+     */
+    public SVGOMAnimatedNumberList(AbstractElement elt,
+                                   String ns,
+                                   String ln,
+                                   String defaultValue) {
+        
+        element = elt;
+        namespaceURI = ns;
+        localName = ln;
+        this.defaultValue = defaultValue;
+    }
+    
+    /**
+     * return the SVGNumberList mapping
+     * the static attribute
+     * of the element
+     *
+     * @return a number list.
+     */
+    public SVGNumberList getBaseVal(){
+        if ( numbers == null ){
+            numbers = new SVGOMNumberList();
+        }
+         return numbers;
+    }
+
+    /**
+     */
+    public SVGNumberList getAnimVal(){
+        throw new RuntimeException("TODO :  getAnimVal() !!");
+    }
+    
+    /**
+     * Called when an Attr node has been added.
+     */
+    public void attrAdded(Attr node, String newv) {
+        if (!changing && numbers != null) {
+            numbers.invalidate();
+        }
+    }
+
+    /**
+     * Called when an Attr node has been modified.
+     */
+    public void attrModified(Attr node, String oldv, String newv) {
+        if (!changing && numbers != null) {
+            numbers.invalidate();
+        }
+    }
+
+    /**
+     * Called when an Attr node has been removed.
+     */
+    public void attrRemoved(Attr node, String oldv) {
+        if (!changing && numbers != null) {
+            numbers.invalidate();
+        }
+    }
+    
+    /**
+     * SVGNumberList implementation.
+     */
+    public class SVGOMNumberList extends AbstractSVGNumberList {
+
+        public SVGOMNumberList(){
+            super();
+        }
+
+        /**
+         * Create a DOMException.
+         */
+        protected DOMException createDOMException(short    type,
+                                                  String   key,
+                                                  Object[] args){
+            return element.createDOMException(type,key,args);
+        }
+
+        /**
+         * Create a SVGException.
+         */
+        protected SVGException createSVGException(short    type,
+                                                  String   key,
+                                                  Object[] args){
+
+            return ((SVGOMElement)element).createSVGException(type,key,args);
+        }
+
+        /**
+         */
+        protected Element getElement(){
+            return element;
+        }
+
+        /**
+         * Retrieve the value of the attribute 'points'.
+         */
+        protected String getValueAsString(){
+            Attr attr = element.getAttributeNodeNS(namespaceURI, localName);
+            if (attr == null) {
+                return defaultValue;
+            }
+            return attr.getValue();
+        }
+
+        /**
+         * Set the value of the attribute 'points'
+         */
+        protected void setAttributeValue(String value){
+            try{
+                changing = true;
+                element.setAttributeNS(namespaceURI, localName, value);
+            }
+            finally{
+                changing = false;
+            }
+        }
+    }
+}
