11677767e8998458bec5343fa9bd95af8624e12c
Vincent Hardy <vhardy@apache.org>
2004-08-18 07:17:26 +0000

    Applied Apache License Version 2.0 by following the instructions at http://www.apache.org/dev/apply-license.html.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@201374 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/rasterizertask/build.bat
+++ contrib/rasterizertask/build.bat
@@ -1,10 +1,19 @@
 @ECHO OFF
-REM --------------------------------------------------------------------------
-REM  Copyright (C) The Apache Software Foundation. All rights reserved.        
-REM  ------------------------------------------------------------------------- 
-REM  This software is published under the terms of the Apache Software License 
-REM  version 1.1, a copy of which has been included with this distribution in  
-REM  the LICENSE file.                                                         
+REM ----------------------------------------------------------------------------
+REM Copyright 2001 The Apache Software Foundation
+REM 
+REM    Licensed under the Apache License, Version 2.0 (the "License");
+REM    you may not use this file except in compliance with the License.
+REM    You may obtain a copy of the License at
+REM 
+REM        http://www.apache.org/licenses/LICENSE-2.0
+REM 
+REM    Unless required by applicable law or agreed to in writing, software
+REM    distributed under the License is distributed on an "AS IS" BASIS,
+REM    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM    See the License for the specific language governing permissions and
+REM    limitations under the License.
+REM 
 REM --------------------------------------------------------------------------
 
 REM --------------------------------------------------------------------------
