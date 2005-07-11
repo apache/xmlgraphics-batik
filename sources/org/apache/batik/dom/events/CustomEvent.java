3f83a8ae656cfb77c3833cf07d1efb04cd1d2631
Cameron McCormack <cam@apache.org>
2005-07-11 05:33:56 +0000

    Forgot Apache licence.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/branches/svg12@210072 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/dom/events/CustomEvent.java
+++ sources/org/apache/batik/dom/events/CustomEvent.java
@@ -1,3 +1,20 @@
+/*
+
+   Copyright 2000-2001  The Apache Software Foundation 
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
 package org.apache.batik.dom.events;
 
 /**
@@ -16,6 +33,9 @@ package org.apache.batik.dom.events;
  *   does not implement this interface, reflection will be used to access
  *   the needed methods.
  * </p>
+ *
+ * @author <a href="mailto:cam%40mcc%2eid%2eau">Cameron McCormack</a>
+ * @version $Id$
  */
 public interface CustomEvent extends org.w3c.dom.events.CustomEvent {
 
