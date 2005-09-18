8eb91cc0e2d7a28a9cac6358c80ce6b4b24411bd
Cameron McCormack <cam@apache.org>
2005-09-18 07:36:32 +0000

    1. Fixed problem with binding custom elements deep in a tree selected    with xbl:content.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@289905 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/bridge/svg12/ContentManager.java
+++ sources/org/apache/batik/bridge/svg12/ContentManager.java
@@ -371,7 +371,7 @@ public class ContentManager {
             added.addAll(newlySelectedNodes);
             added.removeAll(previouslySelectedNodes);
 
-            if (!first) {            
+            if (!first) {
                 xblManager.shadowTreeSelectedContentChanged(removed, added);
             }
         }
