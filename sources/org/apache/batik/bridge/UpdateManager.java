f4775feab4874b90eadc2db77d1de12b591d8d4c
Thomas E. DeWeese <deweese@apache.org>
2006-03-10 12:18:04 +0000

    1) Updates to docs.    a) Alphabatized projects list.    b) Added GLIPS Graffiti. 2) Update Manager's MIN_REPAINT time is now adjustable. 3) GraphicsUtil warning about Graphics from BufferedImage    w/o BUFFERED_IMAGE hint is now controllable with property.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@384772 13f79535-47bb-0310-9956-ffa450edef68

--- sources/org/apache/batik/bridge/UpdateManager.java
+++ sources/org/apache/batik/bridge/UpdateManager.java
@@ -51,13 +51,13 @@ import org.w3c.dom.events.EventTarget;
  */
 public class UpdateManager  {
 
-    static final long MIN_REPAINT_TIME;
+    static final int MIN_REPAINT_TIME;
     static {
-        long value = 20;
+        int value = 20;
         try {
             String s = System.getProperty
             ("org.apache.batik.min_repaint_time", "20");
-            value = Long.parseLong(s);
+            value = Integer.parseInt(s);
         } catch (SecurityException se) {
         } catch (NumberFormatException nfe){
         } finally {
@@ -137,6 +137,11 @@ public class UpdateManager  {
     protected ScriptingEnvironment[] secondaryScriptingEnvironments;
 
     /**
+     * The current minRepaintTime
+     */
+    protected int minRepaintTime;
+
+    /**
      * Creates a new update manager.
      * @param ctx The bridge context.
      * @param gn GraphicsNode whose updates are to be tracked.
@@ -175,6 +180,15 @@ public class UpdateManager  {
             ScriptingEnvironment se = initializeScriptingEnvironment(resCtx);
             secondaryScriptingEnvironments[i] = se;
         }
+        minRepaintTime = MIN_REPAINT_TIME;
+    }
+
+    public int getMinRepaintTime() {
+        return minRepaintTime;
+    }
+
+    public void setMinRepaintTime(int minRepaintTime) {
+        this.minRepaintTime = minRepaintTime;
     }
 
     /**
@@ -518,7 +532,7 @@ public class UpdateManager  {
         if (!updateTracker.hasChanged()) 
             return;
         long ctime = System.currentTimeMillis();
-        if (ctime-outOfDateTime < MIN_REPAINT_TIME) {
+        if (ctime-outOfDateTime < minRepaintTime) {
             // We very recently did a repaint check if other 
             // repaint runnables are pending.
             synchronized (updateRunnableQueue.getIteratorLock()) {
