e2b3dd9d35cefd053869a361a7a2c2877b1df4b1
Luis Bernardo <lbernardo@apache.org>
2015-03-04 14:05:42 +0000

    fixed permissions in policy files; removed manifest files that are not needed; renamed pdf transcoder jar
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/branches/submodules_cyclic_deps@1664003 13f79535-47bb-0310-9956-ffa450edef68

--- builddeps.sh
+++ builddeps.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm -rf checkdeps
 mkdir checkdeps
 ls -l batik-svn-trunk/maven/batik-*/*/*.jar | awk '{printf("cp %s checkdeps/\n", $9)}' > cp.sh
@@ -6,7 +7,7 @@ chmod +x cp.sh
 cp lib/*.jar checkdeps
 rm cp.sh
 cd checkdeps
-/home/lbernardo/Downloads/jdk1.8.0_20/bin/jdeps -dotoutput dot -classpath . *
+jdeps -dotoutput dot -classpath . *
 grep batik dot/batik* | grep jar | grep -v 'java\.' | grep -v javax | grep -v xml-apis | grep -v xalan | grep -v JDK | grep -v Path | grep -v '{' | awk '{print $1 " -> " $5}' | sort -u | sed s/'dot\/'/'"'/g | sed s/'.dot:'/'"'/g | sed s/'('/'"'/g | sed s/')"'/'"'/g  > deps.dot
 echo 'digraph "batik-jars-deps" {' > batikdeps.dot
 cat deps.dot >> batikdeps.dot
