6397b09e25d122e72c8800ef61fe8663d2da45f1
Simon Steiner <ssteiner@apache.org>
2017-09-11 09:51:29 +0000

    Make headers the same
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/batik/trunk@1808001 13f79535-47bb-0310-9956-ffa450edef68

--- batik-svg-dom/src/main/java/org/apache/batik/dom/svg/SVGAnimationContext.java
+++ batik-svg-dom/src/main/java/org/apache/batik/dom/svg/SVGAnimationContext.java
@@ -15,7 +15,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 
-*/
+ */
 package org.apache.batik.dom.svg;
 
 import org.w3c.dom.smil.ElementTimeControl;
