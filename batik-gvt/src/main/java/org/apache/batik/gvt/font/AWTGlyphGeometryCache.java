ef5b651a9ecead0306b31073b93068d00fc4c848
Simon Steiner <ssteiner@apache.org>
2025-11-19 12:08:10 +0000

    BATIK-1271: Make AWTGlyphGeometryCache thread safe

--- batik-gvt/src/main/java/org/apache/batik/gvt/font/AWTGlyphGeometryCache.java
+++ batik-gvt/src/main/java/org/apache/batik/gvt/font/AWTGlyphGeometryCache.java
@@ -18,10 +18,10 @@
  */
 package org.apache.batik.gvt.font;
 
+import org.apache.xmlgraphics.image.loader.util.SoftMapCache;
+
 import java.awt.Shape;
 import java.awt.geom.Rectangle2D;
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.SoftReference;
 
 /**
  * This class represents a doubly indexed hash table, which holds
@@ -32,63 +32,14 @@ import java.lang.ref.SoftReference;
  * @version $Id$
  */
 public class AWTGlyphGeometryCache {
-
-    /**
-     * The initial capacity
-     */
-    protected static final int INITIAL_CAPACITY = 71;
-
-    /**
-     * The underlying array
-     */
-    protected Entry[] table;
-
-    /**
-     * The number of entries
-     */
-    protected int count;
-
-    /**
-     * The reference queue.
-     */
-    protected ReferenceQueue referenceQueue = new ReferenceQueue();
-
-    /**
-     * Creates a new AWTGlyphGeometryCache.
-     */
-    public AWTGlyphGeometryCache() {
-        table = new Entry[INITIAL_CAPACITY];
-    }
-
-    /**
-     * Creates a new AWTGlyphGeometryCache.
-     * @param c The inital capacity.
-     */
-    public AWTGlyphGeometryCache(int c) {
-        table = new Entry[c];
-    }
-
-    /**
-     * Returns the size of this table.
-     */
-    public int size() {
-        return count;
-    }
+    private SoftMapCache table = new SoftMapCache(true);
 
     /**
      * Gets the value of a variable
      * @return the value or null
      */
     public Value get(char c) {
-        int hash  = hashCode(c) & 0x7FFFFFFF;
-        int index = hash % table.length;
-
-        for (Entry e = table[index]; e != null; e = e.next) {
-            if ((e.hash == hash) && e.match(c)) {
-                return (Value)e.get();
-            }
-        }
-        return null;
+        return (Value) table.get(c);
     }
 
     /**
@@ -96,103 +47,15 @@ public class AWTGlyphGeometryCache {
      * @return the old value or null
      */
     public Value put(char c, Value value) {
-        removeClearedEntries();
-
-        int hash  = hashCode(c) & 0x7FFFFFFF;
-        int index = hash % table.length;
-
-        Entry e = table[index];
-        if (e != null) {
-            if ((e.hash == hash) && e.match(c)) {
-                Object old = e.get();
-                table[index] = new Entry(hash, c, value, e.next);
-                return (Value)old;
-            }
-            Entry o = e;
-            e = e.next;
-            while (e != null) {
-                if ((e.hash == hash) && e.match(c)) {
-                    Object old = e.get();
-                    e = new Entry(hash, c, value, e.next);
-                    o.next = e;
-                    return (Value)old;
-                }
-
-                o = e;
-                e = e.next;
-            }
-        }
-
-        // The key is not in the hash table
-        int len = table.length;
-        if (count++ >= (len - (len >> 2))) {
-            // more than 75% loaded: grow
-            rehash();
-            index = hash % table.length;
-        }
-
-        table[index] = new Entry(hash, c, value, table[index]);
-        return null;
+        table.put(c, value);
+        return value;
     }
 
     /**
      * Clears the table.
      */
     public void clear() {
-        table = new Entry[INITIAL_CAPACITY];
-        count = 0;
-        referenceQueue = new ReferenceQueue();
-    }
-
-    /**
-     * Rehash the table
-     */
-    protected void rehash () {
-        Entry[] oldTable = table;
-
-        table = new Entry[oldTable.length * 2 + 1];
-
-        for (int i = oldTable.length-1; i >= 0; i--) {
-            for (Entry old = oldTable[i]; old != null;) {
-                Entry e = old;
-                old = old.next;
-
-                int index = e.hash % table.length;
-                e.next = table[index];
-                table[index] = e;
-            }
-        }
-    }
-
-    /**
-     * Computes a hash code corresponding to the given objects.
-     */
-    protected int hashCode(char c) {
-        return c;
-    }
-
-    /**
-     * Removes the cleared entries.
-     */
-    protected void removeClearedEntries() {
-        Entry e;
-        while ((e = (Entry)referenceQueue.poll()) != null) {
-            int index = e.hash % table.length;
-            Entry t = table[index];
-            if (t == e) {
-                table[index] = e.next;
-            } else {
-                loop: for (;t!=null;) {
-                    Entry c = t.next;
-                    if (c == e) {
-                        t.next = e.next;
-                        break loop;
-                    }
-                    t = c;
-                }
-            }
-            count--;
-        }
+        table.clear();
     }
 
     /**
@@ -232,44 +95,6 @@ public class AWTGlyphGeometryCache {
          */
         public Rectangle2D getOutlineBounds2D() {
             return outlineBounds;
-        }
-    }
-
-    /**
-     * To manage collisions
-     */
-    protected class Entry extends SoftReference {
-
-        /**
-         * The hash code
-         */
-        public int hash;
-
-        /**
-         * The character
-         */
-        public char c;
-
-        /**
-         * The next entry
-         */
-        public Entry next;
-
-        /**
-         * Creates a new entry
-         */
-        public Entry(int hash, char c, Value value, Entry next) {
-            super(value, referenceQueue);
-            this.hash  = hash;
-            this.c  = c;
-            this.next  = next;
-        }
-
-        /**
-         * Whether this entry match the given keys.
-         */
-        public boolean match(char o2) {
-            return (c == o2);
         }
     }
 }
